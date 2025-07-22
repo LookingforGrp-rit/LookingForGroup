514af91c3810469ad7d3c731761ac045d9bea51b
I1ect <picept9@gmail.com>
2025-07-22 13:22:42 -0400

    finished for now
    It is currently in a workable state but still missing a few key features

--- client/src/components/Profile/ProfileContactTab.tsx
+++ client/src/components/Profile/ProfileContactTab.tsx
@@ -1,7 +1,6 @@
 import { useState, useEffect } from "react";
 import { getUsersById, getUsers } from "../../api/users";
-import '../Styles/profileContactTab.css'
-
+import "../Styles/profileContactTab.css";
 
 interface LinkData {
   id: Number;
@@ -9,87 +8,103 @@ interface LinkData {
 }
 
 const ProfileContactTab = (userID: number) => {
-    let links = [] as LinkData[];
-    const [socials, setSocials] = useState([]);
+  let links = [] as LinkData[];
+  const [socials, setSocials] = useState([]);
 
-    let linksTSX;
-    const [firstName, setFirstName] = useState("Joe")
-    const [lastName, setLastName] = useState("Bob")
-    const [email, setEmail] = useState("dummy@gmail.com");
-    const [phone, setPhone] = useState("1-431-dummy");
+  let linksTSX;
+  const [firstName, setFirstName] = useState("Joe");
+  const [lastName, setLastName] = useState("Bob");
+  const [email, setEmail] = useState("dummy@gmail.com");
+  const [phone, setPhone] = useState("1-431-dummy");
 
-
-
-    // copied over from Profile.tsx to get social links
-    useEffect(() => {
+  // copied over from Profile.tsx to get social links
+  useEffect(() => {
     const loadSocials = async () => {
-        try {
-          const response = await getUsersById(1) // Currently still testing, grabs id #!
-          const user = response.data;
-          console.log("Response: ", response.status)
-          if (response.status === 200) {
-            setFirstName(user.data.first_name)
-            setLastName(user.data.last_name)
-            setSocials(user.socials)
-            setEmail(user.data.email); // Not Yet Exists
-            setPhone(user.data.phone); // Not Yet Exists
-            console.log("success!")
-          }
+      try {
+        const response = await getUsersById(1); // Currently still testing, grabs id #!
+        const user = response.data;
+        console.log("Response: ", response.status);
+        if (response.status === 200) {
+          setFirstName(user.firstName);
+          setLastName(user.lastName);
+          setSocials(user.socials);
+          setEmail(user.data.email); // Not Yet Exists
+          setPhone(user.data.phone); // Not Yet Exists
+          console.log("success!");
+        }
 
-      console.log("response data:" + response)
+        console.log("response data:" + response);
 
-      // Setup links
+        // Setup links
 
-      if (socials) {
-        links = socials.map((s: any) => {
-          return {
-            id: s.id,
-            url: s.url
-          };
-        });
+        if (socials) {
+          links = socials.map((s: any) => {
+            return {
+              id: s.id,
+              url: s.url,
+            };
+          });
+        }
+
+        if (links) {
+          linksTSX = links.map((link) => {
+            if (link.url.includes("linkedin.com"))
+              return (
+                <p key={link.id} className="linkedin-social-link">
+                  <a href={link.url.toString()}>{link.url}</a>
+                </p>
+              );
+            else if (link.url.includes("instagram.com"))
+              return (
+                <p key={link.id} className="instagram-social-link">
+                  <a href={link.url.toString()}>{link.url}</a>
+                </p>
+              );
+            else
+              return (
+                <p className="other-social-link">
+                  {" "}
+                  <a key={link.id} href={link.url.toString()}>
+                    {link.url}
+                  </a>
+                </p>
+              );
+          });
+        }
+      } catch (err) {
+        console.log("failed to load socials", err);
       }
-      
-      if (links){
-        linksTSX = links.map((link) =>{  
-            if (link.url.includes("linkedin.com")) return ( <p key = {link.id} className = "linkedin-social-link"><a href = {link.url.toString()}>{link.url}</a></p> );
-                else if (link.url.includes("instagram.com")) return (<p key = {link.id} className = "instagram-social-link"><a href = {link.url.toString()}>{link.url}</a></p>);
-                else return (<p className = "other-social-link">  <a key = {link.id} href = {link.url.toString()}>{link.url}</a></p>);
-            }
-        )};
-
-    } catch (err){console.log('failed to load socials', err);}
-
-    }
+    };
     loadSocials();
   }, []);
- 
-    return(
-        <div className="profile-contact-tab">
-            <h1>Contact Info</h1>
-            <div className="contact-info-list">
-                {firstName && (
-                    <div className="name"> 
-                        <p>Name: {firstName + " " + lastName}</p>
-                    </div>                    
-                )}
 
-                {email && (
-                    <div className="email-links"> 
-                        <p>Email: {email}</p>
-                    </div>                    
-                )}
-                
-                { phone && (
-                    <div className="phone-link"> 
-                        <p>Phone Number: <a href = {phone}>{phone}</a></p>
-                    </div>
-                )}
-                { links && (
-                    <div className="links">{linksTSX}</div>
-                )}  
-            </div>
-        </div>
-    );
-}
+  return (
+    <div className="profile-contact-tab">
+      <h1>Contact Info</h1>
+      <div className="contact-info-list">
+        {firstName && (
+          <div className="name">
+            <p>Name: {firstName + " " + lastName}</p>
+          </div>
+        )}
+
+        {email && (
+          <div className="email-links">
+            <p>Email: {email}</p>
+          </div>
+        )}
+
+        {phone && (
+          <div className="phone-link">
+            <p>
+              Phone Number: <a href={phone}>{phone}</a>
+            </p>
+          </div>
+        )}
+        {links && <div className="links">{linksTSX}</div>}
+      </div>
+    </div>
+  );
+};
 
 export default ProfileContactTab;
