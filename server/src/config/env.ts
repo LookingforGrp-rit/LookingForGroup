e40f766480b65196237a7a3b7ce41fcde311715f
Thomas <77514919+TB516@users.noreply.github.com>
2025-07-25 14:36:40 -0400

    Update env.ts

--- server/src/config/env.ts
+++ server/src/config/env.ts
@@ -2,9 +2,9 @@ const envConfig = Object.freeze({
   env: (process.env.NODE_ENV ?? 'production') as 'production' | 'development' | 'test',
   databaseUrl: `mysql://${process.env.DB_USER}:${process.env.DB_PASS}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`,
   s3Endpoint: `${process.env.S3_HOST}:${process.env.S3_PORT}`,
-  s3Bucket: process.env.S3_BUCKET,
-  s3User: process.env.S3_USER,
-  s3Pass: process.env.S3_PASS,
+  s3Bucket: `${process.env.S3_BUCKET}`,
+  s3User: `${process.env.S3_USER}`,
+  s3Pass: `${process.env.S3_PASS}`,
   port: process.env.PORT ?? 3000,
 });
 
