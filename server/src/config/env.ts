a4ddf68b45b10cfd835a76111f91a014de140986
Thomas <77514919+TB516@users.noreply.github.com>
2025-07-25 14:15:51 -0400

    fixed typing in env.ts

--- server/src/config/env.ts
+++ server/src/config/env.ts
@@ -1,5 +1,5 @@
 const envConfig = Object.freeze({
-  env: process.env.NODE_ENV ?? ('production' as 'production' | 'development' | 'test'),
+  env: (process.env.NODE_ENV ?? 'production') as 'production' | 'development' | 'test',
   databaseUrl: `mysql://${process.env.DB_USER}:${process.env.DB_PASS}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`,
   port: process.env.PORT ?? 3000,
 });
