53841de303a0095c43081f76563aed8521d38504
Elia Cohen <mrc6113@rit.edu>
2025-08-27 15:31:43 -0400

    middleware to pull current user from shibboleth and add it to the request

---
+++ server/src/api/middleware/inject-current-user.ts
@@ -0,0 +1,50 @@
+import type { ApiResponse } from '@looking-for-group/shared';
+import type { NextFunction, Request, Response } from 'express';
+import { uidHeaderKey } from '#config/constants.ts';
+import { getUserByShibService } from '#services/users/get-user-shib.ts';
+
+const injectCurrentUser = async (request: Request, response: Response, next: NextFunction) => {
+  const universityId = request.headers[uidHeaderKey] as string | undefined;
+
+  //if no university id found
+  if (!universityId) {
+    const resBody: ApiResponse = {
+      status: 400,
+      error: 'Missing university ID in headers',
+      data: null,
+      memetype: 'application/json',
+    };
+    response.status(400).json(resBody);
+    return;
+  }
+
+  const result = await getUserByShibService(universityId);
+
+  if (result === 'INTERNAL_ERROR') {
+    const resBody: ApiResponse = {
+      status: 500,
+      error: 'Internal Server Error',
+      data: null,
+      memetype: 'application/json',
+    };
+    response.status(500).json(resBody);
+    return;
+  }
+
+  if (result === 'NOT_FOUND') {
+    const resBody: ApiResponse = {
+      status: 404,
+      error: 'User not found',
+      data: null,
+      memetype: 'application/json',
+    };
+    response.status(404).json(resBody);
+    return;
+  }
+
+  const userID = result.userId;
+  request.currentUser = `${userID}`;
+  next();
+};
+
+export default injectCurrentUser;
