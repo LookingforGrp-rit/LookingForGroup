04b99e86cb20a31566bf4bf3d19a2ea8c2bee1bb
Elia Cohen <mrc6113@rit.edu>
2025-08-29 18:25:06 -0400

    implement delete member

--- server/src/api/controllers/projects/delete-member.ts
+++ server/src/api/controllers/projects/delete-member.ts
@@ -1,54 +1,60 @@
 import type { ApiResponse } from '@looking-for-group/shared';
 import type { Request, Response } from 'express';
+import { deleteMemberService } from '#services/projects/delete-member.ts';
 
-const deleteMemberController = (req: Request, res: Response) => {
-  res.status(501).json({
-    status: 501,
-    error: 'NOT IMPLEMENTED',
-    data: null,
-    memetype: 'application/json',
-  } as ApiResponse);
-
-  // const { id } = req.params;
-  // const memberId = parseInt(id);
-  // if (isNaN(memberId)) {
-  //   const resBody: ApiResponse = {
-  //     status: 400,
-  //     error: 'Invalid member ID',
-  //     data: null,
-  //     memetype: 'application/json',
-  //   };
-  //   res.status(400).json(resBody);
-  //   return;
-  // }
-  // const result = await deleteService(memberId);
-  // if (result === 'NOT_FOUND') {
-  //   const resBody: ApiResponse = {
-  //     status: 404,
-  //     error: 'Member not found',
-  //     data: null,
-  //     memetype: 'application/json',
-  //   };
-  //   res.status(404).json(resBody);
-  //   return;
-  // }
-  // if (result === 'INTERNAL_ERROR') {
-  //   const resBody: ApiResponse = {
-  //     status: 500,
-  //     error: 'Internal Server Error',
-  //     data: null,
-  //     memetype: 'application/json',
-  //   };
-  //   res.status(500).json(resBody);
-  //   return;
-  // }
-  // const resBody: ApiResponse = {
-  //   status: 200,
-  //   error: null,
-  //   data: result,
+const deleteMemberController = async (req: Request, res: Response) => {
+  // res.status(501).json({
+  //   status: 501,
+  //   error: 'NOT IMPLEMENTED',
+  //   data: null,
   //   memetype: 'application/json',
-  // };
-  // res.status(200).json(resBody);
+  // } as ApiResponse);
+
+  const { id } = req.params;
+  const { userId } = req.params;
+  const projectId = parseInt(id);
+  const memberId = parseInt(userId);
+
+  if (isNaN(projectId) || isNaN(memberId)) {
+    const resBody: ApiResponse = {
+      status: 400,
+      error: 'Invalid project or member id',
+      data: null,
+      memetype: 'application/json',
+    };
+    res.status(400).json(resBody);
+    return;
+  }
+
+  const result = await deleteMemberService(projectId, memberId);
+
+  if (result === 'NOT_FOUND') {
+    const resBody: ApiResponse = {
+      status: 404,
+      error: 'Member not found',
+      data: null,
+      memetype: 'application/json',
+    };
+    res.status(404).json(resBody);
+    return;
+  }
+  if (result === 'INTERNAL_ERROR') {
+    const resBody: ApiResponse = {
+      status: 500,
+      error: 'Internal Server Error',
+      data: null,
+      memetype: 'application/json',
+    };
+    res.status(500).json(resBody);
+    return;
+  }
+  const resBody: ApiResponse = {
+    status: 200,
+    error: null,
+    data: result,
+    memetype: 'application/json',
+  };
+  res.status(200).json(resBody);
 };
 
 export default deleteMemberController;
---
+++ server/src/services/projects/delete-member.ts
@@ -0,0 +1,35 @@
+import type { Member } from '@looking-for-group/shared';
+import prisma from '#config/prisma.ts';
+import type { ServiceErrorSubset } from '#services/service-error.ts';
+
+type DeleteFollowServiceError = ServiceErrorSubset<'INTERNAL_ERROR' | 'NOT_FOUND'>;
+
+//delete a user following
+export const deleteMemberService = async (
+  projectId: number,
+  memberId: number,
+): Promise<Member | DeleteFollowServiceError> => {
+  try {
+    //delete the user being followed
+    const deleteMember = await prisma.members.delete({
+      where: {
+        projectId_userId: {
+          projectId,
+          userId: memberId,
+        },
+      },
+    });
+
+    return deleteMember;
+  } catch (error) {
+    console.error('Error in deleteMemberService:', error);
+
+    if (error instanceof Object && 'code' in error) {
+      if (error.code === 'P2025') {
+        return 'NOT_FOUND';
+      }
+    }
+
+    return 'INTERNAL_ERROR';
+  }
+};
