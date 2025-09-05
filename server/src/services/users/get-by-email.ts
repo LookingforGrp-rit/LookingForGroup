7c8578dffea537ae09afb210c7d26b7cb6f0b662
Elia Cohen <mrc6113@rit.edu>
2025-09-05 18:55:36 -0400

    update get by email to use new user preview selector and transformer

--- server/src/services/users/get-by-email.ts
+++ server/src/services/users/get-by-email.ts
@@ -1,6 +1,8 @@
 import type { UserPreview } from '@looking-for-group/shared';
 import prisma from '#config/prisma.ts';
+import { UserPreviewSelector } from '#services/selectors/users/user-preview.ts';
 import type { ServiceErrorSubset } from '#services/service-outcomes.ts';
+import { transformUserToPreview } from '#services/transformers/users/user-preview.ts';
 
 type GetUserServiceError = ServiceErrorSubset<'INTERNAL_ERROR' | 'NOT_FOUND'>;
 
@@ -12,19 +14,13 @@ export const getUserByEmailService = async (
     //should be unique
     const user = await prisma.users.findFirst({
       where: { ritEmail },
-      select: {
-        userId: true,
-        firstName: true,
-        lastName: true,
-        username: true,
-        profileImage: true,
-      },
+      select: UserPreviewSelector,
     });
 
     if (!user) return 'NOT_FOUND';
 
     //return the transformed user
-    return user;
+    return transformUserToPreview(user);
   } catch (e) {
     console.error(`Error in getUserByUsernameService: ${JSON.stringify(e)}`);
     return 'INTERNAL_ERROR';
