d942e03316bef00a30af1baa74723e7739ab177d
Elia Cohen <mrc6113@rit.edu>
2025-09-05 17:59:31 -0400

    new endpoints for socials and mediums implemented (after fixing my side)

--- server/src/services/me/update-info.ts
+++ server/src/services/me/update-info.ts
@@ -1,8 +1,10 @@
+import type { MePrivate } from '@looking-for-group/shared';
 import prisma from '#config/prisma.ts';
 import type { Users } from '#prisma-models/index.js';
 import { deleteImageService } from '#services/images/delete-image.ts';
-//import { MePrivateSelector } from '#services/selectors/me/me-private.ts';
+import { MePrivateSelector } from '#services/selectors/me/me-private.ts';
 import type { ServiceErrorSubset } from '#services/service-outcomes.ts';
+import { transformMeToPrivate } from '#services/transformers/me/me-private.ts';
 
 type UpdateUserServiceError = ServiceErrorSubset<'INTERNAL_ERROR' | 'NOT_FOUND'>;
 
@@ -29,7 +31,7 @@ type UpdatebleUserFields = Partial<
 export const updateUserInfoService = async (
   userId: number,
   updates: UpdatebleUserFields,
-): Promise<UpdatebleUserFields | UpdateUserServiceError> => {
+): Promise<MePrivate | UpdateUserServiceError> => {
   try {
     const curUser = await prisma.users.findFirst({
       where: {
@@ -48,12 +50,11 @@ export const updateUserInfoService = async (
 
     const user = await prisma.users.update({
       where: { userId },
-      data: {
-        ...updates,
-      },
+      data: { ...updates },
+      select: MePrivateSelector,
     });
 
-    return user;
+    return transformMeToPrivate(user);
   } catch (e) {
     console.error('Error in updateUserInfoService:', e);
     return 'INTERNAL_ERROR';
