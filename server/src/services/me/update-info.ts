6b8d2ee49992916c6e5d1f8ff981711012219bc0
Elia Cohen <mrc6113@rit.edu>
2025-09-05 18:38:35 -0400

    revert edits on update-info

--- server/src/services/me/update-info.ts
+++ server/src/services/me/update-info.ts
@@ -2,14 +2,11 @@ import type { MePrivate } from '@looking-for-group/shared';
 import prisma from '#config/prisma.ts';
 import type { Users } from '#prisma-models/index.js';
 import { deleteImageService } from '#services/images/delete-image.ts';
-import { uploadImageService } from '#services/images/upload-image.ts';
 import { MePrivateSelector } from '#services/selectors/me/me-private.ts';
 import type { ServiceErrorSubset } from '#services/service-outcomes.ts';
 import { transformMeToPrivate } from '#services/transformers/me/me-private.ts';
 
-type UpdateUserServiceError = ServiceErrorSubset<
-  'INTERNAL_ERROR' | 'NOT_FOUND' | 'CONTENT_TOO_LARGE'
->;
+type UpdateUserServiceError = ServiceErrorSubset<'INTERNAL_ERROR' | 'NOT_FOUND'>;
 
 // updatable fields only
 type UpdatebleUserFields = Partial<
@@ -20,6 +17,7 @@ type UpdatebleUserFields = Partial<
     | 'headline'
     | 'pronouns'
     | 'title'
+    | 'profileImage'
     | 'academicYear'
     | 'location'
     | 'funFact'
@@ -28,7 +26,7 @@ type UpdatebleUserFields = Partial<
     | 'username'
     | 'phoneNumber'
   >
-> & { profileImage?: Express.Multer.File };
+>;
 
 export const updateUserInfoService = async (
   userId: number,
@@ -43,33 +41,17 @@ export const updateUserInfoService = async (
 
     if (!curUser) return 'NOT_FOUND';
 
-    let newProfileImage = undefined;
-
     if (updates.profileImage) {
       const oldProfileImage = curUser.profileImage;
-
-      const result = await uploadImageService(
-        updates.profileImage.buffer,
-        updates.profileImage.originalname,
-        updates.profileImage.mimetype,
-      );
-      if (result === 'INTERNAL_ERROR') return 'INTERNAL_ERROR';
-      if (result === 'CONTENT_TOO_LARGE') return 'CONTENT_TOO_LARGE';
-
-      newProfileImage = result.location;
-
       if (oldProfileImage !== null) {
         await deleteImageService(oldProfileImage);
       }
     }
 
-    const databaseUpdates: Omit<UpdatebleUserFields, 'profileImage'> = updates;
-
     const user = await prisma.users.update({
       where: { userId },
       data: {
-        ...databaseUpdates,
-        ...(newProfileImage ? { profileImage: newProfileImage } : {}),
+        ...updates,
       },
       select: MePrivateSelector,
     });
