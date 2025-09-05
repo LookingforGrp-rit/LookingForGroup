47fddf2ef735d1be67a7ea01fdd120528b93d14b
Elia Cohen <mrc6113@rit.edu>
2025-09-05 17:53:07 -0400

    update get socials to use the new my social transformer

--- server/src/services/me/get-socials.ts
+++ server/src/services/me/get-socials.ts
@@ -1,21 +1,22 @@
+import type { MySocial } from '@looking-for-group/shared';
 import prisma from '#config/prisma.ts';
-import type { UserSocials } from '#prisma-models/index.js';
+import { MySocialSelector } from '#services/selectors/me/my-social.ts';
 import type { ServiceErrorSubset } from '#services/service-outcomes.ts';
+import { transformMySocial } from '#services/transformers/me/my-social.ts';
 
 type GetSocialsError = ServiceErrorSubset<'INTERNAL_ERROR' | 'NOT_FOUND'>;
 
-export const getSocialsService = async (
-  userId: number,
-): Promise<UserSocials[] | GetSocialsError> => {
+export const getSocialsService = async (userId: number): Promise<MySocial[] | GetSocialsError> => {
   try {
     //all their socials
     const socials = await prisma.userSocials.findMany({
       where: {
         userId: userId,
       },
+      select: MySocialSelector,
     });
 
-    return socials;
+    return socials.map(transformMySocial);
   } catch (e) {
     console.error(`Error in getSocialsService: ${JSON.stringify(e)}`);
     return 'INTERNAL_ERROR';
