e3eb7acf446136131f342fe172a5e2d737d3d517
Elia Cohen <mrc6113@rit.edu>
2025-09-05 19:06:36 -0400

    update get user followers to use the new user preview selector and transformer

--- server/src/services/users/get-user-followers.ts
+++ server/src/services/users/get-user-followers.ts
@@ -1,20 +1,22 @@
-import type { UserFollowings } from '@looking-for-group/shared';
+import type { UserPreview } from '@looking-for-group/shared';
 import prisma from '#config/prisma.ts';
+import { UserPreviewSelector } from '#services/selectors/users/user-preview.ts';
 import type { ServiceErrorSubset } from '#services/service-outcomes.ts';
+import { transformUserToPreview } from '#services/transformers/users/user-preview.ts';
 
 type GetUserFollowerServiceError = ServiceErrorSubset<'INTERNAL_ERROR' | 'NOT_FOUND'>;
 
 //get the user by the username
 export const getUserFollowersService = async (
   receiverId: number,
-): Promise<UserFollowings[] | GetUserFollowerServiceError> => {
+): Promise<UserPreview[] | GetUserFollowerServiceError> => {
   try {
     const following = await prisma.userFollowings.findMany({
       where: { receiverId: receiverId },
       select: {
-        senderId: true,
-        receiverId: true,
-        followedAt: true,
+        senderUser: {
+          select: UserPreviewSelector,
+        },
       },
     });
 
@@ -22,7 +24,7 @@ export const getUserFollowersService = async (
       return 'NOT_FOUND';
     }
 
-    return following;
+    return following.map(({ senderUser }) => transformUserToPreview(senderUser));
   } catch (e) {
     console.error(`Error in getUserFollowingService: ${JSON.stringify(e)}`);
     return 'INTERNAL_ERROR';
