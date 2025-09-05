d5d27cdd5159661666b98c03bd85bb82f847fcf0
Elia Cohen <mrc6113@rit.edu>
2025-09-05 17:49:34 -0400

    bring user into the 21st century (add validation and 404 checks)

--- server/src/services/me/delete-user.ts
+++ server/src/services/me/delete-user.ts
@@ -1,7 +1,22 @@
 import prisma from '#config/prisma.ts';
+import type { ServiceErrorSubset, ServiceSuccessSusbet } from '#services/service-outcomes.ts';
 
-export const deleteUserService = async (userId: number): Promise<string> => {
+type DeleteUserServiceError = ServiceErrorSubset<'INTERNAL_ERROR' | 'NOT_FOUND'>;
+type DeleteUserServiceSuccess = ServiceSuccessSusbet<'NO_CONTENT'>;
+
+export const deleteUserService = async (
+  userId: number,
+): Promise<DeleteUserServiceError | DeleteUserServiceSuccess> => {
   try {
+    //user validation (does this user exist)
+    const userExists = await prisma.userSocials.findFirst({
+      where: {
+        userId,
+      },
+    });
+
+    if (!userExists) return 'NOT_FOUND';
+
     await Promise.all([
       prisma.userFollowings.deleteMany({ where: { senderId: userId } }),
       prisma.userFollowings.deleteMany({ where: { receiverId: userId } }),
@@ -11,7 +26,7 @@ export const deleteUserService = async (userId: number): Promise<string> => {
       prisma.users.delete({ where: { userId } }),
     ]);
 
-    return '';
+    return 'NO_CONTENT';
   } catch (e) {
     console.error(`Error in deleteUserService: ${JSON.stringify(e)}`);
     return 'INTERNAL_ERROR';
