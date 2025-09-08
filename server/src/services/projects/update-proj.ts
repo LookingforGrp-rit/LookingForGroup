3af5b5e771124fedd2c7b548af9abe2678d1f23d
Elia Cohen <mrc6113@rit.edu>
2025-09-08 05:17:47 -0400

    update update project to return updated project using the new project detail selector and transformer

--- server/src/services/projects/update-proj.ts
+++ server/src/services/projects/update-proj.ts
@@ -1,14 +1,17 @@
+import type { ProjectDetail } from '@looking-for-group/shared';
 import prisma from '#config/prisma.ts';
 import type { Prisma } from '#prisma-models/index.js';
 import { deleteImageService } from '#services/images/delete-image.ts';
+import { ProjectDetailSelector } from '#services/selectors/projects/project-detail.ts';
 import type { ServiceErrorSubset } from '#services/service-outcomes.ts';
+import { transformProjectToDetail } from '#services/transformers/projects/project-detail.ts';
 
 type UpdateProjectServiceError = ServiceErrorSubset<'INTERNAL_ERROR' | 'NOT_FOUND'>;
 
 const updateProjectService = async (
   projectId: number,
   updates: Prisma.ProjectsUpdateInput,
-): Promise<ReturnType<typeof prisma.projects.update> | UpdateProjectServiceError> => {
+): Promise<ProjectDetail | UpdateProjectServiceError> => {
   try {
     const curProject = await prisma.projects.findFirst({
       where: {
@@ -24,9 +27,10 @@ const updateProjectService = async (
     const project = await prisma.projects.update({
       where: { projectId },
       data: updates,
+      select: ProjectDetailSelector,
     });
 
-    return project;
+    return transformProjectToDetail(project);
   } catch (e) {
     console.error('Error in updateProjectService:', e);
     return 'INTERNAL_ERROR';
