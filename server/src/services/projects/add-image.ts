a2676481e6f621c305a1005d392a1e8f181a70b0
Elia Cohen <mrc6113@rit.edu>
2025-09-08 03:27:56 -0400

    update add image with new image type

--- server/src/services/projects/add-image.ts
+++ server/src/services/projects/add-image.ts
@@ -3,6 +3,7 @@ import prisma from '#config/prisma.ts';
 import type { Prisma } from '#prisma-models/index.js';
 import { ProjectImageSelector } from '#services/selectors/projects/parts/project-image.ts';
 import type { ServiceErrorSubset } from '#services/service-outcomes.ts';
+import { transformProjectImage } from '#services/transformers/projects/project-image.ts';
 
 type AddImageServiceError = ServiceErrorSubset<'INTERNAL_ERROR'>;
 
@@ -10,9 +11,12 @@ const addImageService = async (
   data: Prisma.ProjectImagesCreateInput,
 ): Promise<ProjectImage | AddImageServiceError> => {
   try {
-    const result = await prisma.projectImages.create({ data, select: ProjectImageSelector });
+    const newImage = await prisma.projectImages.create({
+      data,
+      select: { ...ProjectImageSelector, projectId: true },
+    });
 
-    return result;
+    return transformProjectImage(newImage.projectId, newImage);
   } catch (e) {
     console.error('Error in addImageService:', e);
     return 'INTERNAL_ERROR';
