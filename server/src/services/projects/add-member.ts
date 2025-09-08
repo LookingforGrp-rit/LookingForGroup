2e95b30c03622f18138d50d115fea4752e220e63
Elia Cohen <mrc6113@rit.edu>
2025-09-08 03:44:18 -0400

    update add member to use new selector and transformer

--- server/src/services/projects/add-member.ts
+++ server/src/services/projects/add-member.ts
@@ -1,14 +1,25 @@
+import type { ProjectMember } from '@looking-for-group/shared';
 import prisma from '#config/prisma.ts';
 import type { Prisma } from '#prisma-models/index.js';
+import { ProjectMemberSelector } from '#services/selectors/projects/parts/project-member.ts';
 import type { ServiceErrorSubset } from '#services/service-outcomes.ts';
+import { transformProjectMember } from '#services/transformers/projects/project-member.ts';
 
 type AddMemberServiceError = ServiceErrorSubset<'INTERNAL_ERROR' | 'NOT_FOUND' | 'CONFLICT'>;
 
 const addMemberService = async (
   data: Prisma.MembersCreateInput,
-): Promise<Prisma.MembersCreateManyRolesInput | AddMemberServiceError> => {
+): Promise<ProjectMember | AddMemberServiceError> => {
   try {
-    const result = await prisma.members.create({ data });
+    const newMember = await prisma.members.create({
+      data,
+      select: {
+        ...ProjectMemberSelector,
+        projectId: true,
+      },
+    });
+    const result = transformProjectMember(newMember.projectId, newMember);
+
     return result;
   } catch (e) {
     if (e instanceof Object && 'code' in e) {
