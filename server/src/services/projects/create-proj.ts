e495b9b981e10094eb34b36e3f7f0101841fbffe
Elia Cohen <mrc6113@rit.edu>
2025-09-08 04:37:50 -0400

    update create project to use new project detail selector and transformer

--- server/src/services/projects/create-proj.ts
+++ server/src/services/projects/create-proj.ts
@@ -1,16 +1,19 @@
+import type { ProjectDetail } from '@looking-for-group/shared';
 import prisma from '#config/prisma.ts';
 import type { Prisma } from '#prisma-models/index.js';
+import { ProjectDetailSelector } from '#services/selectors/projects/project-detail.ts';
 import type { ServiceErrorSubset } from '#services/service-outcomes.ts';
+import { transformProjectToDetail } from '#services/transformers/projects/project-detail.ts';
 
 type CreateProjectServiceError = ServiceErrorSubset<'INTERNAL_ERROR' | 'NOT_FOUND'>;
 
 const createProjectService = async (
   data: Prisma.ProjectsCreateInput,
-): Promise<ReturnType<typeof prisma.projects.create> | CreateProjectServiceError> => {
+): Promise<ProjectDetail | CreateProjectServiceError> => {
   try {
-    const project = await prisma.projects.create({ data });
+    const project = await prisma.projects.create({ data, select: ProjectDetailSelector });
 
-    return project;
+    return transformProjectToDetail(project);
   } catch (e) {
     console.error('Error in createProjectService:', e);
     return 'INTERNAL_ERROR';
