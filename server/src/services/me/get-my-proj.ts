672b264510eb8962e48165f2b9ed0a0755382895
Elia Cohen <mrc6113@rit.edu>
2025-09-08 05:23:39 -0400

    update get my projects to return project previews using the new project preview selector and transformer

--- server/src/services/me/get-my-proj.ts
+++ server/src/services/me/get-my-proj.ts
@@ -1,29 +1,32 @@
-import type { ProjectWithFollowers } from '@looking-for-group/shared';
+import type { ProjectPreview } from '@looking-for-group/shared';
 import prisma from '#config/prisma.ts';
+import { ProjectPreviewSelector } from '#services/selectors/projects/project-preview.ts';
 import type { ServiceErrorSubset } from '#services/service-outcomes.ts';
-import { transformProject } from '../transformers/projects/projectTransform.ts';
+import { transformProjectToPreview } from '#services/transformers/projects/project-preview.ts';
 
 type GetProjectsError = ServiceErrorSubset<'INTERNAL_ERROR' | 'NOT_FOUND'>;
 
 export const getMyProjectsService = async (
   userId: number,
-): Promise<ProjectWithFollowers[] | GetProjectsError> => {
+): Promise<ProjectPreview[] | GetProjectsError> => {
   try {
     //all the projects they own
     const ownedProjects = await prisma.projects.findMany({
       where: {
         userId: userId,
       },
+      select: ProjectPreviewSelector,
     });
     //all the projects they're a member of
     const memberProjects = await prisma.projects.findMany({
       where: {
         members: {
-          every: {
+          some: {
             userId: userId,
           },
         },
       },
+      select: ProjectPreviewSelector,
     });
 
     //all the projects
@@ -33,7 +36,7 @@ export const getMyProjectsService = async (
     if (projects.length === 0) return 'NOT_FOUND';
 
     //user helper to transform project
-    const fullProject = projects.map(transformProject);
+    const fullProject = projects.map(transformProjectToPreview);
 
     return fullProject;
   } catch (e) {
