562c69042b2ececc00c68c56dd9ab34b7b2920df
Elia Cohen <mrc6113@rit.edu>
2025-09-12 04:32:36 -0400

    update types to remove undefined types and limit nulls to only necessary nulls

--- shared/types.d.ts
+++ shared/types.d.ts
@@ -24,7 +24,26 @@ export type SkillProficiency =
     | "Novice"
     | "Intermediate"
     | "Advanced"
-    | "Expert"
+    | "Expert";
+export type ProjectPurpose = 
+    | "Personal"
+    | "Portfolio Piece"
+    | "Academic"
+    | "Co-Op";
+export type JobAvailability =
+    | "Full Time"
+    | "Part Time"
+    | "Flexible";
+export type JobDuration = 
+    | "Short Term"
+    | "Long Term";
+export type JobLocation = 
+    | "On Site"
+    | "Remote"
+    | "Hybrid";
+export type JobCompensation = 
+    | "Unpaid"
+    | "Paid";
 
 
 //API REQUEST
@@ -136,20 +155,20 @@ export interface UserPreview {
     firstName: string;
     lastName: string;
     username: string;
-    profileImage?: string | null;
+    profileImage: string | null;
     apiUrl: string;
 }
 
 //show only non-sensitive data
 export interface UserDetail extends UserPreview {
-    headline: string | null;
-    pronouns: string | null;
-    title: string | null;
+    headline: string;
+    pronouns: string;
+    title: string;
     majors: Major[];
-    academicYear: string | null;
-    location: string | null;
-    funFact: string | null;
-    bio?: string | null;
+    academicYear: AcademicYear | null;
+    location: string;
+    funFact: string;
+    bio: string;
     projects: ProjectPreview[];
     skills: UserSkill[];
     socials: UserSocial[];
@@ -158,32 +177,14 @@ export interface UserDetail extends UserPreview {
 }
 
 //all user private data
-export interface User {
-    userId: number;
-    username: string;
+export interface User extends UserDetail {
     ritEmail: string;
-    firstName: string;
-    lastName: string;
-    profileImage?: string | null;
-    headline: string | null;
-    pronouns: string | null;
-    title: string | null;
-    academicYear: string | null;
-    location: string | null;
-    funFact: string | null;
-    bio: string | null;
     visibility: Visibility;
     projects: ProjectPreview[];
-    majors?: Major[] | null;
-    skills?: UserSkill[] | null;
-    socials?: Social[] | null;
     phoneNumber: string | null;
-    universityId: string | null;
+    universityId: string;
     createdAt: Date;
     updatedAt: Date;
-    following: {usersFollowing: UserFollowsList, projectsFollowing: ProjectFollowsList},
-    followers: UserFollowsList;
-    apiUrl: string;
 }
 
 // Represents the member info for a project
@@ -202,20 +203,20 @@ export interface MePreview {
     firstName: string;
     lastName: string;
     username: string;
-    profileImage?: string | null;
+    profileImage: string | null;
     apiUrl: string;
 }
 
 //show only non-sensitive data
 export interface MeDetail extends MePreview {
-    headline: string | null;
-    pronouns: string | null;
-    title: string | null;
+    headline: string;
+    pronouns: string;
+    title: string;
     majors: MyMajor[];
-    academicYear: string | null;
-    location: string | null;
-    funFact: string | null;
-    bio: string | null;
+    academicYear: AcademicYear;
+    location: string;
+    funFact: string;
+    bio: string;
     projects: ProjectPreview[];
     skills?: MySkill[];
     socials?: MySocial[];
@@ -228,7 +229,7 @@ export interface MePrivate extends MeDetail {
     ritEmail: string;
     visibility: Visibility;
     phoneNumber: string | null;
-    universityId: string | null;
+    universityId: string;
     createdAt: Date;
     updatedAt: Date;
 }
@@ -237,15 +238,7 @@ export interface MePrivate extends MeDetail {
 export interface CreateUserData {
     firstName: string;
     lastName: string;
-    headline?: string;
-    pronouns?: string;
-    title?: string;
-    academicYear?: number;
-    location?: string;
-    funFact?: string;
-    majors?: Major[];
-    skills?: UserSkill[];
-    socials?: Social[];
+    username: string;
 }
 
 // PROjECT DATA
@@ -309,10 +302,10 @@ export interface ProjectTag extends Tag {
 export interface ProjectJob {
     jobId: number;
     role: Role;
-    availability: string;
-    duration: string;
-    location: string;
-    compensation: string;
+    availability: JobAvailability;
+    duration: JobDuration;
+    location: JobLocation;
+    compensation: JobCompensation;
     description: string;
     createdAt: Date;
     updatedAt: Date;
@@ -323,7 +316,7 @@ export interface ProjectJob {
 
 export interface ProjectDetail extends ProjectPreview {
     description: string;
-    purpose: string | null;
+    purpose: ProjectPurpose | null;
     status: string | null;
     audience: string | null;
     createdAt: Date;
@@ -356,11 +349,11 @@ export interface Job {
     jobId: number;
     projectId: number;
     roleId: number;
-    availability: string;
-    duration: string;
-    location: string;
-    compensation: string;
-    description?: string;
+    availability: JobAvailability;
+    duration: JobDuration;
+    location: JobLocation;
+    compensation: JobCompensation;
+    description: string;
     createdAt: Date;
     updatedAt: Date;
     apiUrl: string;
