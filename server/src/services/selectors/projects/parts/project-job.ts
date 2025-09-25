a2748db745e5db98408f9e5a09b176ee9f10be09
Elia Cohen <mrc6113@rit.edu>
2025-09-25 14:38:49 -0400

    update project job selectors and transformers

--- server/src/services/selectors/projects/parts/project-job.ts
+++ server/src/services/selectors/projects/parts/project-job.ts
@@ -1,16 +1,20 @@
+import { RoleSelector } from '#services/selectors/datasets/role.ts';
+import { UserPreviewSelector } from '#services/selectors/users/user-preview.ts';
+
 export const ProjectJobSelector = Object.freeze({
   jobId: true,
   roles: {
-    select: {
-      roleId: true,
-      label: true,
-    },
+    select: RoleSelector,
   },
   availability: true,
   duration: true,
   location: true,
   compensation: true,
-  contact: true,
+  contact: {
+    select: {
+      users: UserPreviewSelector,
+    },
+  },
   description: true,
   createdAt: true,
   updatedAt: true,
--- shared/types.d.ts
+++ shared/types.d.ts
@@ -314,7 +314,7 @@ export interface ProjectJob {
     duration: JobDuration;
     location: JobLocation;
     compensation: JobCompensation;
-    contact: string;
+    contact: UserPreview;
     description: string;
     createdAt: Date;
     updatedAt: Date;
