e076f816588822dab81748dff4c2308cee7eb2dd
Elia Cohen <mrc6113@rit.edu>
2025-10-15 17:43:46 -0400

    get-username doesn't have devid support yet

--- client/src/api/users.ts
+++ client/src/api/users.ts
@@ -9,14 +9,19 @@ import type {
   MePrivate,
   MySocial,
   MySkill,
-  Major, Skill, Role, Medium, Tag, Social,
+  Major,
+  Skill,
+  Role,
+  Medium,
+  Tag,
+  Social,
   UpdateUserInput,
   AddUserSocialInput,
   UpdateUserSocialInput,
   AddUserSkillsInput,
   UpdateUserSkillInput,
   AddUserMajorInput,
-  MyMajor
+  MyMajor,
 } from "@looking-for-group/shared";
 
 /* USER CRUD */
@@ -40,11 +45,22 @@ export const createNewUser = async (
  * @returns ApiResponse with username is logged in, 404 if guest
  */
 export const getCurrentUsername = async (): Promise<ApiResponse> => {
-  const apiURL = `/me/get-username`;
+  // const apiURL = `/me/get-username`;
+  const apiURL = `/me`;
   const response = await GET(apiURL);
 
   console.log(response);
-  return response;
+  return {
+    status: response.status,
+    data:
+      response.status === 200
+        ? {
+            userId: (response.data as MePrivate).userId,
+            username: (response.data as MePrivate).username,
+          }
+        : null,
+    error: response.error,
+  };
 };
 
 /**
@@ -65,7 +81,9 @@ export const getUsers = async (): Promise<ApiResponse<UserPreview[]>> => {
  * @param id - database id for user
  * @returns result - JSONified data of specified user.
  */
-export const getUsersById = async (id: string): Promise<ApiResponse<UserDetail>> => {
+export const getUsersById = async (
+  id: string
+): Promise<ApiResponse<UserDetail>> => {
   const apiURL = `/users/${id}`;
   const response = await GET(apiURL);
 
@@ -73,15 +91,14 @@ export const getUsersById = async (id: string): Promise<ApiResponse<UserDetail>>
   return response;
 };
 
-
-//Gets the current user by ID 
+//Gets the current user by ID
 export const getCurrentUserById = async (): Promise<ApiResponse<MePrivate>> => {
   const apiURL = `/me`;
   const response = await GET(apiURL);
 
   console.log(response);
   return response;
-}
+};
 
 /**
  * Edit information for one user, specified by URL.
@@ -89,21 +106,20 @@ export const getCurrentUserById = async (): Promise<ApiResponse<MePrivate>> => {
  * @returns response data
  */
 export const editUser = async (
-  userData: UpdateUserInput,
+  userData: UpdateUserInput
 ): Promise<ApiResponse<MePrivate>> => {
   const apiURL = `/me`;
   const form = new FormData();
-  
-  for (const [name, value] of Object.entries(userData)){
-    if(value !== null) form.append(name, value);
+
+  for (const [name, value] of Object.entries(userData)) {
+    if (value !== null) form.append(name, value);
   }
 
   const response = await PATCH(apiURL, form);
 
-  if(response.error) console.log(`Error in editUser: ${response.error}`);
+  if (response.error) console.log(`Error in editUser: ${response.error}`);
   return response as ApiResponse<MePrivate>; //it would get mad at me if i didn't do this soooo
 };
-
 
 //Removes a user specified by URL.
 export const deleteUser = async (): Promise<ApiResponse> => {
@@ -116,7 +132,6 @@ export const deleteUser = async (): Promise<ApiResponse> => {
 
 /* ACCOUNT INFO/ PASSWORD RESET*/
 
-
 /* LOOKUP USER */
 
 /**
@@ -124,7 +139,9 @@ export const deleteUser = async (): Promise<ApiResponse> => {
  * @param username - Username of user to be recieved
  * @return data, list of 1 user, or 400 if not successful
  */
-export const getUserByUsername = async (username: string): Promise<ApiResponse<UserPreview>> => {
+export const getUserByUsername = async (
+  username: string
+): Promise<ApiResponse<UserPreview>> => {
   const url = `/users/search-username/${username}`;
   const response = await GET(url);
 
@@ -137,10 +154,12 @@ export const getUserByUsername = async (username: string): Promise<ApiResponse<U
  * @param email - email of user to be recieved
  * @return data, list of 1 user, or 400 if not successful
  */
-export const getUserByEmail = async (email: string): Promise<ApiResponse<UserPreview>> => {
+export const getUserByEmail = async (
+  email: string
+): Promise<ApiResponse<UserPreview>> => {
   const url = `/users/search-email/${email}`;
   const response = await GET(url);
-  
+
   console.log(response);
   return response;
 };
@@ -152,35 +171,40 @@ export const getUserByEmail = async (email: string): Promise<ApiResponse<UserPre
  * @param {number} id - id of the user that we are searching.
  * @returns array of users following, or 400 if unsuccessful.
  */
-export const getUserFollowing = async (id: number): Promise<ApiResponse<UserPreview[]>> => {
+export const getUserFollowing = async (
+  id: number
+): Promise<ApiResponse<UserPreview[]>> => {
   const url = `/users/${id}/followings/people`;
   const response = await GET(url);
-  
+
   console.log(response);
   return response;
 };
 
 /** Get list of users that are following the specified user
-* @param {number} id - id of the user that we are searching.
-*/
+ * @param {number} id - id of the user that we are searching.
+ */
 export const getUserFollowers = async (id: number): Promise<ApiResponse> => {
   const url = `/users/${id}/followers`;
   const response = await GET(url);
-  
+
   console.log(response);
   return response;
-}
+};
 
 /**
  * Follow a person for a user.
  * @param {number} userId - ID of the user being followed
  * @returns 201 if successful, 400 if not
  */
-export const addUserFollowing = async (userId: number): Promise<ApiResponse<UserFollowing>> => {
+export const addUserFollowing = async (
+  userId: number
+): Promise<ApiResponse<UserFollowing>> => {
   const url = `/me/followings/people/${userId}`;
   const response = await POST(url, {});
 
-  if(response.error) console.log(`Error in addUserFollowing: ${response.error}`)
+  if (response.error)
+    console.log(`Error in addUserFollowing: ${response.error}`);
   console.log(response);
   return response as ApiResponse<UserFollowing>;
 };
@@ -200,7 +224,9 @@ export const deleteUserFollowing = async (id: number) => {
 /* PROJECT FOLLOWINGS/VISIBILITY */
 
 //Get the current user's projects
-export const getProjectsByUser = async (): Promise<ApiResponse<ProjectPreview[]>> => {
+export const getProjectsByUser = async (): Promise<
+  ApiResponse<ProjectPreview[]>
+> => {
   const url = `/me/projects`;
   const response = await GET(url);
 
@@ -213,7 +239,9 @@ export const getProjectsByUser = async (): Promise<ApiResponse<ProjectPreview[]>
  * @param userId - user to search
  * @return - array of projects, or 400 if unsuccessful.
  */
-export const getVisibleProjects = async (userId: number): Promise<ApiResponse<ProjectPreview[]>> => {
+export const getVisibleProjects = async (
+  userId: number
+): Promise<ApiResponse<ProjectPreview[]>> => {
   const url = `/users/${userId}/projects`;
   const response = await GET(url);
 
@@ -235,7 +263,8 @@ export const updateProjectVisibility = async (
   const visData = { visibility: _visibility }; //it's expecting this
   const response = await PUT(url, visData);
 
-  if(response.error) console.log(`Error in updateProjectVisibility: ${response.error}`)
+  if (response.error)
+    console.log(`Error in updateProjectVisibility: ${response.error}`);
   console.log(response);
   return response;
 };
@@ -245,13 +274,11 @@ export const updateProjectVisibility = async (
  * @param projectID - ID of the project you're leaving
  * @return 201 if successful, 400 if not
  */
-export const leaveProject = async (
-  projectID: number
-) => {
+export const leaveProject = async (projectID: number) => {
   const url = `me/projects/${projectID}/leave`;
   const response = await DELETE(url);
 
-  if(response.error) console.log(`Error in leaveProject: ${response.error}`)
+  if (response.error) console.log(`Error in leaveProject: ${response.error}`);
   console.log(response);
   return response;
 };
@@ -261,7 +288,9 @@ export const leaveProject = async (
  * @param userId - ID of the user.
  * @returns array of projects, or 400 if error.
  */
-export const getProjectFollowing = async (userId: number): Promise<ApiResponse<ProjectPreview[]>> => {
+export const getProjectFollowing = async (
+  userId: number
+): Promise<ApiResponse<ProjectPreview[]>> => {
   const url = `/users/${userId}/followings/projects`;
   const response = await GET(url);
 
@@ -274,21 +303,26 @@ export const getProjectFollowing = async (userId: number): Promise<ApiResponse<P
  * @param projectId - ID of the project you're following
  * @returns 201 if successful, 400 if not.
  */
-export const addProjectFollowing = async (projectId: number): Promise<ApiResponse<ProjectFollowing>> => {
+export const addProjectFollowing = async (
+  projectId: number
+): Promise<ApiResponse<ProjectFollowing>> => {
   const url = `/me/followings/projects/${projectId}`;
   const response = await POST(url, {});
 
-  if(response.error) console.log(`Error in addProjectFollowing: ${response.error}`)
+  if (response.error)
+    console.log(`Error in addProjectFollowing: ${response.error}`);
   console.log(response);
   return response as ApiResponse<ProjectFollowing>;
-}
+};
 
 /**
  * Unfollow a project for a user.
  * @param projectId - ID of the project you're unfollowing
  * @returns 200 if successful, 400 if not.
  */
-export const deleteProjectFollowing = async (projectId: number): Promise<ApiResponse> => {
+export const deleteProjectFollowing = async (
+  projectId: number
+): Promise<ApiResponse> => {
   const url = `/me/followings/projects/${projectId}`;
   const response = await DELETE(url);
 
@@ -306,14 +340,16 @@ export const getUserSocials = async (): Promise<ApiResponse<MySocial[]>> => {
 };
 
 // Add socials for the current user by ID
-/** 
+/**
  * @param socialData - Data used to add the social
  */
-export const addUserSocials = async (socialData: AddUserSocialInput): Promise<ApiResponse<MySocial>> => {
+export const addUserSocials = async (
+  socialData: AddUserSocialInput
+): Promise<ApiResponse<MySocial>> => {
   const apiURL = `/me/socials`;
   const response = await POST(apiURL, socialData);
 
-  if(response.error) console.log(`Error in addUserSocials: ${response.error}`)
+  if (response.error) console.log(`Error in addUserSocials: ${response.error}`);
   console.log(response);
   return response as ApiResponse<MySocial>;
 };
@@ -323,26 +359,32 @@ export const addUserSocials = async (socialData: AddUserSocialInput): Promise<Ap
  * @param websiteId - ID of the social to be updated
  * @param socialData - Data used to update the social
  */
-export const updateUserSocials = async (websiteId: number, socialData: UpdateUserSocialInput): Promise<ApiResponse<MySocial>> => {
+export const updateUserSocials = async (
+  websiteId: number,
+  socialData: UpdateUserSocialInput
+): Promise<ApiResponse<MySocial>> => {
   const apiURL = `/me/socials/${websiteId}`;
   const response = await PATCH(apiURL, socialData);
 
-  if(response.error) console.log(`Error in updateUserSocials: ${response.error}`)
+  if (response.error)
+    console.log(`Error in updateUserSocials: ${response.error}`);
   console.log(response);
   return response as ApiResponse<MySocial>;
 };
 
 // Delete user socials
 /**
-* @param websiteId - ID of the social to be deleted
-*/
-export const deleteUserSocials = async (websiteId: number): Promise<ApiResponse> => {
+ * @param websiteId - ID of the social to be deleted
+ */
+export const deleteUserSocials = async (
+  websiteId: number
+): Promise<ApiResponse> => {
   const url = `/me/socials/${websiteId}`;
   const response = await DELETE(url);
 
   console.log(response);
-  return response
-}
+  return response;
+};
 
 // Get skills for the current user based on ID
 export const getUserSkills = async (): Promise<ApiResponse<MySkill[]>> => {
@@ -353,8 +395,8 @@ export const getUserSkills = async (): Promise<ApiResponse<MySkill[]>> => {
   return response;
 };
 
-// Add a skill to the current user 
-/** 
+// Add a skill to the current user
+/**
  * @param skillData - Data with which to add a skill
  */
 export const addUserSkills = async (
@@ -363,40 +405,42 @@ export const addUserSkills = async (
   const url = `/me/skills`;
   const response = await POST(url, skillData);
 
-  if(response.error) console.log(`Error in addUserSkills: ${response.error}`)
+  if (response.error) console.log(`Error in addUserSkills: ${response.error}`);
   console.log(response);
   return response as ApiResponse<MySkill>;
-}
+};
 
 // Updates a user skill
-/** 
+/**
  * @param skillId - ID of the skill to be updated
  * @param skillData - Data with which to update the skill
-*/
+ */
 export const updateUserSkills = async (
-  skillId: number, 
-  skillData: UpdateUserSkillInput,
+  skillId: number,
+  skillData: UpdateUserSkillInput
 ): Promise<ApiResponse<MySkill>> => {
   const url = `/me/skills/${skillId}`;
   const response = await PATCH(url, skillData);
 
-  if(response.error) console.log(`Error in updateUserSkills: ${response.error}`)
+  if (response.error)
+    console.log(`Error in updateUserSkills: ${response.error}`);
   console.log(response);
   return response as ApiResponse<MySkill>;
-} 
+};
 
 // Delete a user skill
-/** 
+/**
  * @param skillId - ID of the skill
-*/
-export const deleteUserSkills = async (skillId: number): Promise<ApiResponse<null>> => {
+ */
+export const deleteUserSkills = async (
+  skillId: number
+): Promise<ApiResponse<null>> => {
   const url = `/me/skills/${skillId}`;
   const response = await DELETE(url);
-  
+
   console.log(response);
   return response as ApiResponse<null>;
 };
-
 
 // Get majors for the current user based on ID
 export const getUserMajors = async (): Promise<ApiResponse<MyMajor[]>> => {
@@ -407,26 +451,28 @@ export const getUserMajors = async (): Promise<ApiResponse<MyMajor[]>> => {
   return response;
 };
 
-// Add a major to the current user 
+// Add a major to the current user
 export const addUserMajor = async (
   majorData: AddUserMajorInput
 ): Promise<ApiResponse<MyMajor>> => {
   const url = `/me/majors`;
   const response = await POST(url, majorData);
 
-  if(response.error) console.log(`Error in addUserMajor: ${response.error}`)
+  if (response.error) console.log(`Error in addUserMajor: ${response.error}`);
   console.log(response);
   return response as ApiResponse<MyMajor>;
-}
+};
 
 // Delete a user major
-/** 
+/**
  * @param majorId - ID of the major to be deleted
-*/
-export const deleteUserMajor = async (majorId: number): Promise<ApiResponse<null>> => {
+ */
+export const deleteUserMajor = async (
+  majorId: number
+): Promise<ApiResponse<null>> => {
   const url = `/me/majors/${majorId}`;
   const response = await DELETE(url);
-  
+
   console.log(response);
   return response as ApiResponse<null>;
 };
@@ -442,13 +488,13 @@ export const getMajors = async (): Promise<ApiResponse<Major[]>> => {
 
   console.log(response);
   return response;
-}
+};
 
 /**
  * Gets list of job titles.
  */
 export const getJobTitles = async (): Promise<ApiResponse<Role[]>> => {
-  const apiURL = `/datasets/roles`
+  const apiURL = `/datasets/roles`;
   const response = await GET(apiURL);
 
   console.log(response);
@@ -459,45 +505,45 @@ export const getJobTitles = async (): Promise<ApiResponse<Role[]>> => {
  * Retrieves list of project types.
  */
 export const getProjectTypes = async (): Promise<ApiResponse<Medium[]>> => {
-  const apiURL = `/datasets/mediums`
+  const apiURL = `/datasets/mediums`;
   const response = await GET(apiURL);
 
   console.log(response);
   return response;
-}
+};
 
 /**
  * Gets list of skills.
  */
 export const getSkills = async (): Promise<ApiResponse<Skill[]>> => {
-  const apiURL = `/datasets/skills`
+  const apiURL = `/datasets/skills`;
   const response = await GET(apiURL);
 
   console.log(response);
   return response;
-}
+};
 
 /**
  * Retrieves list of tags.
  */
 export const getTags = async (): Promise<ApiResponse<Tag[]>> => {
-  const apiURL = `/datasets/tags`
+  const apiURL = `/datasets/tags`;
   const response = await GET(apiURL);
 
   console.log(response);
   return response;
-}
+};
 
 /**
  * Gets list of socials links.
  */
 export const getSocials = async (): Promise<ApiResponse<Social[]>> => {
-  const apiURL = `/datasets/socials`
+  const apiURL = `/datasets/socials`;
   const response = await GET(apiURL);
 
   console.log(response);
   return response;
-}
+};
 
 export default {
   createNewUser,
