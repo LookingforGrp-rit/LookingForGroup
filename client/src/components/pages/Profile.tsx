da360644b993f0501834774e34d3897ab186431e
Elia Cohen <mrc6113@rit.edu>
2025-10-16 15:55:28 -0400

    rename new profile to profile

--- client/src/components/pages/Profile.tsx
+++ client/src/components/pages/Profile.tsx
@@ -1,116 +1,412 @@
-// NOT CURRENTLY IN USE
-import { ProfileHeader } from '../Profile/ProfileHeader';
-// import { ProfileInterests } from '../Profile/ProfileInterests';
-import { ProfileSkills } from '../Profile/ProfileSkills';
-// import { ProfileEndorsements } from '../Profile/ProfileEndorsements';
-import { ProfileProjects } from '../Profile/ProfileProjects';
-import { useEffect, useState } from 'react';
-import ToTopButton from '../ToTopButton';
-import EditButton from '../Profile/ProfileEditButton';
-import { getUsers, getUsersById } from '../../api/users';
+//Styles
+import "../Styles/credits.css";
+import "../Styles/discoverMeet.css";
+import "../Styles/emailConfirmation.css";
+import "../Styles/general.css";
+import "../Styles/loginSignup.css";
+import "../Styles/profile.css";
+import "../Styles/projects.css";
+import "../Styles/settings.css";
+import "../Styles/pages.css";
 
-// Get user ID from API
-// NOTE: name used to match same api call as getCurrentUsername(). Not to be confused.
-const fetchUserID = async () => {
-  // Auth: replaced with shibboleth
-  const response = await fetch('/api/auth');
-  const {
-    data: { userID },
-  } = await response.json();
-  return userID;
-};
+import { useState, useEffect, useCallback } from "react";
+import { useNavigate } from "react-router-dom";
+import * as paths from "../../constants/routes";
+import { Header, loggedIn } from "../Header";
+import { PanelBox } from "../PanelBox";
+import { ProfileEditPopup } from "../Profile/ProfileEditPopup";
+import { Dropdown, DropdownButton, DropdownContent } from "../Dropdown";
+import { ThemeIcon } from "../ThemeIcon";
+// import { ProfileInterests } from "../Profile/ProfileInterests";
+import profilePicture from "../../images/blue_frog.png";
+import { getVisibleProjects, getProjectsByUser, getCurrentAccount } from "../../api/users";
+import { getUsersById } from "../../api/users";
+import { MeDetail, UserSkill, ProjectPreview } from '@looking-for-group/shared';
+import usePreloadedImage from "../../functions/imageLoad";
+import { Input } from "../Input";
+import LabelInputBox from "../LabelInputBox";
 
-// Get list of users on site from API
-const getProfiles = async () => {  
-  const response = await getUsers();
-  return response.data;
-};
+//backend base url for getting images
+const API_BASE = `http://localhost:8081`;
 
-// Main Profile component
-const Profile = (props) => {
-  const [profileID, setProfileID] = useState<string | null>(null);
-  const [profiles, setProfiles] = useState<{ _id: number; username: string }[]>([]);
+type Profile = MeDetail;
+type Tag = UserSkill;
+type Project = ProjectPreview;
 
-  //Get profile id from search query
+// Stores if profile is loaded from server and if it's user's respectively
+// const [profileLoaded, setProfileLoaded] = useState(false);
+let userID: number;
+let isUsersProfile: boolean = true; //FIXME: undo hard-code for debugging
+
+// Change this when follow backend is added, this is just for testing purposes
+let toggleFollow = false;
+
+const NewProfile = () => {
+  // --------------------
+  // Global variables
+  // --------------------
+
+  // FIXME what is the purpose of the default profile? was this just for debugging?
+  // if the user exists, the default profile isn't necessary
+  // if the user doesn't exist, then the page should be the 404 page
+  const defaultProfile: MeDetail = {
+    firstName: "Private",
+    lastName: "User",
+    username: "privateuser",
+    profileImage: `private.webp`,
+    headline: `This user is private`,
+    pronouns: "NA/NA",
+    title: "NA",
+    majors: [],
+    academicYear: "Freshman",
+    location: "NA, NA",
+    funFact: ``,
+    bio: "",
+    skills: [],
+    mentor: false,
+    socials: [],
+    projects: [],
+    following: {
+      usersFollowing: {
+        users: [],
+        count: 0,
+        apiUrl: ""
+      },
+      projectsFollowing: {
+        projects: [],
+        count: 0,
+        apiUrl: ""
+      }
+    },
+    followers: {
+      users: [],
+      count: 0,
+      apiUrl: ""
+    },
+    userId: 0,
+    designer: false,
+    developer: false,
+    apiUrl: ""
+  };
+
+  const navigate = useNavigate(); // Hook for navigation
+
+  // Get URL parameters to tell what user we're looking for and store it
   const urlParams = new URLSearchParams(window.location.search);
-  setProfileID(urlParams.get('profID'));
+  // User ID of profile being viewed
+  const profileID: string = urlParams.get("userID")!;
 
-  //If nothing is found, use a default id
-  useEffect(() => {
-    if (profileID === null) {
-      (async () => {
-        // Get profile ID from API
-        const userID = await fetchUserID();
-        setProfileID(userID);
-        console.log('userID is: ' + userID);
-        console.log('profileID is: ' + profileID);
-      })();
+  const [displayedProfile, setDisplayedProfile] = useState(defaultProfile);
+
+  // Stores all projects
+  const [fullProjectList, setFullProjectList] = useState<ProjectPreview[]>([]);
+  // Projects displayed for searches
+  const [displayedProjects, setDisplayedProjects] = useState<ProjectPreview[]>([]);
+
+  const projectSearchData = fullProjectList?.map(
+    (project: Project) => {
+      return { name: project.title, description: project.hook };
     }
+  );
 
-    // Get all profiles
-    const fetchProfiles = async () => {
-      const profileData = await getProfiles();
-      setProfiles(profileData);
-    };
-    fetchProfiles();
-  }, [profileID]);
+  // --------------------
+  // Helper functions
+  // --------------------
 
-  //Find profile data using id & assign it to a value to use
-  const profileData = profiles.find((p) => p._id === Number(profileID)) || profiles[0];
+  // 'Follow' button
+  const followUser = () => {
+    const followButton = document.getElementById(
+      "profile-follow-button"
+    ) as HTMLButtonElement;
+    toggleFollow = !toggleFollow;
 
-  /*usestates for selectors*/
-  const [UID, setUID] = useState(profileData._id);
-  const user = profiles[UID];
+    if (!loggedIn) {
+      navigate(paths.routes.LOGIN, { state: { from: location.pathname } }); // Redirect if logged out
+    } else {
+      // (Follow behavior would be implemented here)
 
-  window.scrollTo(0, 0);
-
-  
-  const [userData, setUserData] = useState();
-
-  //Get user data from API
-  const getUserData = async (userID: number) => {
-    try {
-      const response = await getUsersById(userID.toString());
-      setUserData(response.data[0]);
-    } catch (error) {
-      console.log(error);
+      if (toggleFollow) {
+        followButton.innerText = "Following";
+        followButton.style.backgroundColor = "Orange";
+        followButton.style.width = "185px";
+      } else {
+        followButton.innerText = "Follow";
+        followButton.style.backgroundColor = "var(--primary-color)";
+        followButton.style.width = "145px";
+      }
     }
   };
-  if (userData === undefined) {
-    getUserData(1);
-  }
 
+  // Search bar doesn't really have a use, so might as well use it for projects
+  const searchProjects = (searchResults: string[]) => {
+    const tempProjList: Project[] = [];
+
+    for (const result of searchResults[0]) {
+      for (const proj of projectSearchData) {
+        if (result === proj) {
+          tempProjList.push(fullProjectList[projectSearchData.indexOf(proj)]);
+          continue;
+        }
+      }
+    }
+
+    // If no projects were found
+    if (tempProjList.length === 0) {
+      setDisplayedProjects([]); // Clear the displayed list
+      console.log("No matching projects found.");
+    } else {
+      setDisplayedProjects(tempProjList);
+    }
+  };
+
+
+  const getProfileProjectData = useCallback(async () => {
+    try {
+      const response = isUsersProfile ? await getProjectsByUser() : await getVisibleProjects(Number(profileID)) as { data: ProjectPreview[] };          // IMPLEMENT PROJECT GETTING
+      const data = response.data;
+      
+      console.log(response);
+
+      // Only update if there's data
+      if (data) {
+        setFullProjectList(data);
+        setDisplayedProjects(data);
+      }
+    } catch (error) {
+      if (error instanceof Error) {
+        console.error(error.message);
+      } else {
+        console.log(`Unknown error: ${error}`);
+      }
+    }
+  }, [profileID, setFullProjectList, setDisplayedProjects]);
+
+  // Gets the profile data
+  useEffect(() => {
+    const getProfileData = async () => {
+      // Get the profileID to pull data for whoever's profile it is
+      const response = await getCurrentAccount();
+      if (response.data) {
+        isUsersProfile = true; // FIXME this will always be true no matter who is signed in 
+      }
+
+      try {
+        const { data } = await getUsersById(profileID ?? "");
+        console.log('user data', data);
+
+        // Only run this if profile data exists for user
+        if (data) {
+          setDisplayedProfile(data);
+          await getProfileProjectData();
+        }
+      } catch (error) {
+        if (error instanceof Error) {
+          console.error(error.message);
+        } else {
+          console.log(`Unknown error: ${error}`);
+        }
+      }
+    };
+    getProfileData();
+    
+  }, [getProfileProjectData, profileID]);
+
+  // --------------------
+  // Components
+  // --------------------
+  const aboutMeButtons = (
+  <>
+    {/* Add social links if present */}
+    {displayedProfile.socials && (
+      <div id="about-me-buttons">
+        {displayedProfile.socials.map((link) => (
+          <button
+            key={link.websiteId}
+            onClick={() => {
+              window.open(link.url, "_blank");
+            }}
+          >
+            <ThemeIcon
+              id={link.label}
+              width={25}
+              height={25}
+              className={"color-fill"}
+              ariaLabel={link.label}
+            />
+          </button>
+        ))}
+      </div>
+    )}
+
+    {/* If the displayed user is the user's profile */}
+    {isUsersProfile ? (
+      // Show edit buttons
+      <ProfileEditPopup />
+    ) : (
+      <>
+      {/* Or, show follow and options buttons */}
+      <ThemeIcon id={'heart'} width={25} height={25} className={'empty-heart'} ariaLabel="follow"/>
+      {/* FIXME: When following is implemented, use this: */}
+      {/* <ThemeIcon id={'heart'} width={25} height={25} className={isFollowing ? 'filled-heart' : 'empty-heart'} /> */}
+      
+      {/* TODO: Implement Share, Block, and Report functionality */}
+      <Dropdown>
+        <DropdownButton>
+          <ThemeIcon id={'menu'} width={25} height={25} className={'color-fill dropdown-menu'} ariaLabel={'More options'}/>
+        </DropdownButton>
+        <DropdownContent rightAlign={true}>
+          <div id="profile-menu-dropdown">
+            <button className="profile-menu-dropdown-button">
+              <ThemeIcon id={'share'} width={27} height={27} className={'mono-fill'} ariaLabel={'Share'}/>
+              Share
+            </button>
+            <button className="profile-menu-dropdown-button">
+              <ThemeIcon id={'cancel'} width={27} height={27} className={'mono-fill'} ariaLabel={'Block'}/>
+              Block
+            </button>
+            <button
+              className="profile-menu-dropdown-button"
+              id="profile-menu-report"
+            >
+              <ThemeIcon id={'warning'} width={27} height={27} ariaLabel={'Report'}/>
+              Report
+            </button>
+          </div>
+        </DropdownContent>
+      </Dropdown>
+      </>
+    )}
+  </>
+  );
+
+  // --------------------
+  // Final component
+  // --------------------
   return (
     <div className="page">
-      {/*selector at the top to switch between users */}
-      <select
-        onChange={(e) => {
-          setUID(Number(e.target.value));
-        }}
-      >
-        {profiles.map((prof) => {
-          return <option value={prof._id}>{prof.username}</option>;
-        })}
-      </select>
-      <div id="profile-page">
-        <ProfileHeader user={user} />
-        {/* <ProfileInterests user={user} /> */}
-        <div>
-          <ProfileSkills user={user} />
-          {/* {userData ? (
-            <ProfileEndorsements user={userData} />
-          ) : (
-            <p>Loading endorsements...</p>
-          )} */}
-        </div>
-        <ProfileProjects user={user} />
-      </div>
-      {userData === undefined ? '' : <EditButton userData={userData} />}
+      {/* Should probably use the search bar for projects I guess? */}
+      <Header
+        dataSets={{ data: fullProjectList }}
+        onSearch={searchProjects}
+        hideSearchBar={true}
+      />
 
-      {/* Scroll To Top button */}
-      <ToTopButton />
+      {/* Checks if we have profile data to use, then determines what to render */}
+      <div id="profile-page-content">
+        {/* New profile display using css grid, will contain all info except for projects */}
+        <div id="profile-information-grid">
+          <img
+            src={usePreloadedImage(`${API_BASE}/images/profiles/${displayedProfile.profileImage}`, profilePicture)}
+            id="profile-image"
+            alt="profile image"
+            onError={(e) => {
+              const profileImg = e.target as HTMLImageElement;
+              profileImg.src = profilePicture;
+            }}
+          />
+
+          <div id="profile-bio">{displayedProfile.headline}</div>
+
+          <div id="profile-info-name">
+            <span id="profile-fullname">
+              {displayedProfile.firstName} {displayedProfile.lastName}
+            </span>
+            @{displayedProfile.username}
+          </div>
+
+          <div id="profile-info-buttons">{aboutMeButtons}</div>
+
+          <div id="profile-info-extras">
+            <div className="profile-extra">
+              <ThemeIcon id={'role'} width={20} height={20} className={'mono-fill'} ariaLabel={'Profession'}/>
+              {displayedProfile.title}
+            </div>
+            <div className="profile-extra">
+              <ThemeIcon id={'major'} width={24} height={24} className={'mono-fill'} ariaLabel={'Major'}/>
+              {displayedProfile.majors?.join(", ")} {displayedProfile.academicYear}
+            </div>
+            <div className="profile-extra">
+              <ThemeIcon id={'location'} width={12} height={16} className={'mono-fill'} ariaLabel={'Location'}/>
+              {displayedProfile.location}
+            </div>
+            <div className="profile-extra">
+              <ThemeIcon id={'pronouns'} width={22} height={22} className={'mono-fill'} ariaLabel={'Pronouns'} />
+              {displayedProfile.pronouns}
+            </div>
+            {/* Only show mentor status if user is a mentor */}
+            {displayedProfile.mentor && 
+              <div className="profile-extra">
+                <ThemeIcon id={'mentor'} width={20} height={20} className={'mono-fill'} ariaLabel={'Mentorship Status'} />
+                Mentor
+              </div>
+            }
+          </div>
+
+          <div id="profile-info-description">{displayedProfile.bio}</div>
+
+          <div id="profile-info-funfact">
+            <span id="fun-fact-start">
+              {displayedProfile.funFact ? "Fun Fact!" : "No Fun Fact (Yet)!"}
+            </span>
+            {displayedProfile.funFact}
+          </div>
+          {/* <div id="profile-info-interest">
+            <ProfileInterests
+              user={{ interests: displayedProfile.interests || [] }}
+              isUsersProfile={isUsersProfile}
+            />
+          </div> */}
+
+          <div id="profile-info-skills">
+            {displayedProfile.skills !== undefined && (
+              /* Will take in a list of tags the user has selected, then */
+              /* use a map function to generate tags to fill this div */
+              displayedProfile.skills.map((tag) => {
+                let category: string;
+                switch (tag.type) {
+                  case "Design":
+                    category = "red";
+                    break;
+                  case "Developer":
+                    category = "yellow";
+                    break;
+                  case "Soft":
+                    category = "purple";
+                    break;
+                  default:
+                    category = "grey";
+                }
+
+                return (
+                  <div
+                    key={`${tag.skill}`}
+                    className={`skill-tag-label label-${category}`}
+                  >
+                    {tag.skill}
+                  </div>
+                );
+              })
+            )}
+          </div>
+        </div>
+
+        <div id="profile-projects">
+          <h2>Projects</h2>
+          {/* Probably fine to use 25 for itemAddInterval */}
+          {displayedProjects ? (
+            <PanelBox
+              category={"projects"}
+              itemList={displayedProjects}
+              itemAddInterval={25}
+              userId={userID}
+            />
+          ) : (
+            <div>No projects to display</div>
+          )}
+        </div>
+      </div>
     </div>
   );
 };
 
-export default Profile;
+export default NewProfile;
