5765ec80f17a4ffdae53167ebbfe4c543dd16ecf
Corie Beale <bealecorie@gmail.com>
2025-10-21 12:59:49 -0400

    progress on tags tab functionality

---
+++ shared/enums.ts
@@ -0,0 +1,69 @@
+// Enums cannot be in .d.ts files
+
+// Enums for better typing
+export enum SkillType {
+  Developer = "Developer",
+  Designer = "Designer",
+  Artist = "Artist",
+  Music = "Music",
+  Soft = "Soft"
+}
+export enum TagType {
+  Creative = "Creative",
+  Technical = "Technical",
+  Games = "Games",
+  Multimedia = "Multimedia",
+  Music = "Music",
+  Other = "Other",
+  DeveloperSkill = "Developer Skill",
+  DesignerSkill = "Designer Skill",
+  SoftSkill = "Soft Skill",
+  Purpose = "Purpose"
+}
+export enum AcademicYear {
+  Freshman = "Freshman",
+  Sophomore = "Sophomore",
+  Junior = "Junior",
+  Senior = "Senior",
+  Graduate = "Graduate"
+};
+export enum Visibility {
+  Public = 0,
+  Private = 1
+};
+export enum SkillProficiency {
+  Novice = "Novice",
+  Intermediate = "Intermediate",
+  Advanced = "Advanced",
+  Expert = "Expert"
+};
+export enum ProjectPurpose {
+  Personal = "Personal",
+  PortfolioPiece = "PortfolioPiece",
+  Academic = "Academic",
+  CoOp = "CoOp"
+};
+export enum ProjectStatus {
+  Planning = "Planning",
+  Development = "Development",
+  PostProduction = "PostProduction",
+  Complete = "Complete"
+}
+export enum JobAvailability {
+  FullTime = "FullTime",
+  PartTime = "PartTime",
+  Flexible = "Flexible"
+}
+export enum JobDuration {
+  ShortTerm = "ShortTerm",
+  LongTerm = "LongTerm"
+}
+export enum JobLocation {
+  OnSite = "OnSite",
+  Remote = "Remote",
+  Hybrid = "Hybrid"
+}
+export enum JobCompensation {
+  Unpaid = "Unpaid",
+  Paid = "Paid"
+};
