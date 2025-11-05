2fc5edba4bf61f4f3f756687099ed59a0dc2b137
Elia Cohen <mrc6113@rit.edu>
2025-11-05 17:13:33 -0500

    fix file image

--- client/src/components/FileImage.tsx
+++ client/src/components/FileImage.tsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 import placeholder from "../images/project_temp.png";
 
 export const FileImage = ({ file, alt }: { file: File; alt: string }) => {
-  const [src, setSrc] = useState("");
+  const [src, setSrc] = useState<string>();
 
   const fileReader = new FileReader();
   fileReader.onload = () => setSrc(fileReader.result as string);
