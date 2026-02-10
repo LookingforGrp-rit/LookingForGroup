c3c2ab0930c054f66aecb64dabec2732c4451f8a
Simplexeon <ryan@duick.com>
2026-02-10 14:41:15 -0500

    Extracted Team Position panel to TeamPositionsPanel.tsx

--- client/src/components/pages/NewProject.tsx
+++ client/src/components/pages/NewProject.tsx
@@ -7,6 +7,7 @@ import { Popup, PopupButton, PopupContent } from "../Popup";
 import profileImage from "../../images/blue_frog.png";
 import { ProjectCarousel } from "../ProjectCarousel";
 import * as paths from "../../constants/routes";
+import { TeamPositionsPanel } from "../TeamPositionsPanel";
 import { ThemeIcon } from "../ThemeIcon";
 import { getByID } from "../../api/projects";
 import {
@@ -17,13 +18,7 @@ import {
 } from "../../api/users";
 import { leaveProject } from "../projectPageComponents/ProjectPageHelper";
 import { MePrivate, ProjectWithFollowers } from "@looking-for-group/shared";
-import {
-  JobAvailability as JobAvailabilityEnums,
-  JobDuration as JobDurationEnums,
-  JobLocation as JobLocationEnums,
-  JobCompensation as JobCompensationEnums,
-  ProjectStatus as ProjectStatusEnums,
-} from "@looking-for-group/shared/enums";
+import { ProjectStatus as ProjectStatusEnums } from "@looking-for-group/shared/enums";
 
 //Main component for the project page
 /**
@@ -49,6 +44,7 @@ const NewProject = () => {
 
   const [followCount, setFollowCount] = useState(0);
   const [isFollowing, setFollowing] = useState(false);
+  const [viewedPosition, setViewedPosition] = useState(0);
 
   /**
    * Checks in the current user is following a project
@@ -374,10 +370,6 @@ const NewProject = () => {
     openOpenPositionsPanel();
   };
 
-
-  //State variable used to track which position is currently being viewed in the popup
-  const [viewedPosition, setViewedPosition] = useState(0);
-
   //Find first member with the job title of 'Project Lead'
   //If no such member exists, use first member in project member list
   const projectLead = displayedProject?.owner;
@@ -424,130 +416,8 @@ const NewProject = () => {
                     Open Positions
                   </PopupButton>
                   <PopupContent>
-                    <div id="project-open-positions-popup">
-                      <div id="positions-popup-header">Join The Team</div>
-
-                      {/* Left Container */}
-                      <div id="project-team-open-positions-popup">
-                        <div className="positions-popup-list">
-                          <p className="positions-popup-info-title">
-                            Open Positions
-                          </p>
-                          <div id="team-positions-popup-list-buttons">
-                            {displayedProject.jobs?.map((job, index) => (
-                              <button
-                                className={`positions-popup-list-item`}
-                                id={
-                                  index === viewedPosition
-                                    ? "positions-popup-list-item-active"
-                                    : ""
-                                }
-                                onClick={() => setViewedPosition(index)}
-                                key={index}
-                              >
-                                {job.role.label}
-                              </button>
-                            ))}
-                          </div>
-                        </div>
-                      </div>
-
-                      {/* Right Container */}
-                      <div className="positions-popup-info-wrapper">
-                        <div className="positions-popup-info">
-                          <div className="positions-popup-info-title">
-                            {displayedProject.jobs[viewedPosition]?.role
-                              ?.label ?? undefined}
-                          </div>
-
-                          <div id="position-description-header">
-                            What we are looking for:
-                          </div>
-
-                          <div
-                            id="position-description-content"
-                            className="positions-popup-info-description"
-                          >
-                            {displayedProject.jobs[viewedPosition]?.description}
-                          </div>
-
-                          <div id="open-position-details">
-                            <div id="open-position-details-left">
-                              <div id="position-availability">
-                                <span className="position-detail-indicator">
-                                  Availability:{" "}
-                                </span>
-                                {
-                                  JobAvailabilityEnums[
-                                    displayedProject.jobs[viewedPosition]
-                                      ?.availability
-                                  ]
-                                }
-                              </div>
-                              <div id="position-location">
-                                <span className="position-detail-indicator">
-                                  Location:{" "}
-                                </span>
-                                {
-                                  JobLocationEnums[
-                                    displayedProject.jobs[viewedPosition]
-                                      ?.location
-                                  ]
-                                }
-                              </div>
-                            </div>
-
-                            <div id="open-position-details-right">
-                              <div id="position-duration">
-                                <span className="position-detail-indicator">
-                                  Duration:{" "}
-                                </span>
-                                {
-                                  JobDurationEnums[
-                                    displayedProject.jobs[viewedPosition]
-                                      ?.duration
-                                  ]
-                                }
-                              </div>
-                              <div id="position-compensation">
-                                <span className="position-detail-indicator">
-                                  Compensation:{" "}
-                                </span>
-                                {
-                                  JobCompensationEnums[
-                                    displayedProject.jobs[viewedPosition]
-                                      ?.compensation
-                                  ]
-                                }
-                              </div>
-                            </div>
-                          </div>
-                        </div>
-
-                        <div id="position-contact">
-                          If interested, please contact:{" "}
-                          <span
-                            onClick={() =>
-                              navigate(
-                                `${paths.routes.PROFILE}?userID=${projectLead?.userId}`
-                              )
-                            }
-                            id="position-contact-link"
-                          >
-                            {/* {FIXME: get project lead profile image in a different way} */}
-                            {/* <img src={(projectLead?.profile_image) 
-                            ? `images/profiles/${projectLead?.profile_image}` 
-                            : profilePicture} 
-                          /> */}
-                            {projectLead?.firstName} {projectLead?.lastName}
-                          </span>
-                        </div>
-                      </div>
-
-                      <PopupButton buttonId="positions-popup-close">
-                        Close
-                      </PopupButton>
-                    </div>
+                    <TeamPositionsPanel displayedProject={displayedProject}
+                        viewedPosition={viewedPosition} setViewedPosition={setViewedPosition} />
                   </PopupContent>
                 </Popup>
               </div>
