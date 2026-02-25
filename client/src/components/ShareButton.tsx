5bbecc742b27925628e5b71d9aafc406dbf4e171
Simplexeon <ryan@duick.com>
2026-02-25 11:00:06 -0500

    Added optional alternate contents to ShareButton

--- client/src/components/ShareButton.tsx
+++ client/src/components/ShareButton.tsx
@@ -1,9 +1,21 @@
-import { useState } from "react";
+import { useState, FC, ReactNode } from "react";
 import { ThemeIcon } from "./ThemeIcon";
 
+interface ShareButtonProps {
+  children?: ReactNode;
+}
 
-export const ShareButton = () => {
+export const ShareButton : FC<ShareButtonProps> = ({children = undefined}) => {
   const [responseVisible, setResponseVisible] = useState<boolean>(false);
+
+  let contents : ReactNode = <>
+    <ThemeIcon id={'share'} width={27} height={27} className={'mono-fill'} ariaLabel={'Share'}/>
+    Share
+  </>;
+
+  if (children != undefined) {
+    contents = children;
+  }
 
   return <>
     <button 
@@ -17,8 +29,7 @@ export const ShareButton = () => {
       <div className={"share-response-popup" + (responseVisible ? "" : " hidden")}>
         Copied!
       </div>
-      <ThemeIcon id={'share'} width={27} height={27} className={'mono-fill'} ariaLabel={'Share'}/>
-      Share
+      {contents}
     </button>
   </>
 }
