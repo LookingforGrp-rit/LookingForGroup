7a09bed41c8cfc9af01380935b3d46ef726fbdf0
Kevin Zheng <kz3175@rit.edu>
2026-05-21 15:33:05 -0400

    fixed project tests

--- server/tests/metest/get-my-proj.test.ts
+++ server/tests/metest/get-my-proj.test.ts
@@ -13,8 +13,8 @@ import { transformProjectToDetail } from '#services/transformers/projects/projec
 
 /* eslint-disable @typescript-eslint/unbound-method */
 /* eslint-disable @typescript-eslint/no-explicit-any */
-
 /* eslint-disable @typescript-eslint/no-unsafe-member-access */
+/* eslint-disable @typescript-eslint/no-unsafe-argument */
 
 vi.mock('#config/prisma.ts', () => ({
   default: {
@@ -33,103 +33,103 @@ vi.mock('#services/transformers/projects/project-detail.ts', () => ({
 
 const now = new Date();
 
+const prismaProjects: ProjectDetail[] = [
+  {
+    apiUrl: '/api/projects/1',
+    audience: '',
+    createdAt: now,
+    description: '',
+    hook: '',
+    jobs: [],
+    mediums: [],
+    members: [],
+    owner: { userId: 1 } as UserPreview,
+    projectId: 1,
+    projectImages: [],
+    projectSocials: [],
+    purpose: 'Academic' as ProjectPurpose,
+    status: 'Planning' as ProjectStatus,
+    tags: [],
+    thumbnail: null,
+    thumbnailId: 0,
+    title: 'test 1',
+    updatedAt: now,
+  },
+  {
+    apiUrl: '/api/projects/2',
+    audience: '',
+    createdAt: now,
+    description: '',
+    hook: '',
+    jobs: [],
+    mediums: [],
+    members: [],
+    owner: { userId: 2 } as UserPreview,
+    projectId: 2,
+    projectImages: [],
+    projectSocials: [],
+    purpose: 'Academic' as ProjectPurpose,
+    status: 'Planning' as ProjectStatus,
+    tags: [],
+    thumbnail: null,
+    thumbnailId: 0,
+    title: 'test 2',
+    updatedAt: now,
+  },
+];
+
+const transformed: ProjectDetail[] = [
+  {
+    apiUrl: '/api/projects/1',
+    audience: '',
+    createdAt: now,
+    description: '',
+    hook: '',
+    jobs: [],
+    mediums: [],
+    members: [],
+    owner: { userId: 1 } as UserPreview,
+    projectId: 1,
+    projectImages: [],
+    projectSocials: [],
+    purpose: 'Academic' as ProjectPurpose,
+    status: 'Planning' as ProjectStatus,
+    tags: [],
+    thumbnail: null,
+    thumbnailId: 0,
+    title: 'test 1',
+    updatedAt: now,
+  },
+  {
+    apiUrl: '/api/projects/2',
+    audience: '',
+    createdAt: now,
+    description: '',
+    hook: '',
+    jobs: [],
+    mediums: [],
+    members: [],
+    owner: { userId: 2 } as UserPreview,
+    projectId: 2,
+    projectImages: [],
+    projectSocials: [],
+    purpose: 'Academic' as ProjectPurpose,
+    status: 'Planning' as ProjectStatus,
+    tags: [],
+    thumbnail: null,
+    thumbnailId: 0,
+    title: 'test 2',
+    updatedAt: now,
+  },
+];
+
 describe('getMyProjectsService', () => {
   beforeEach(() => {
     vi.clearAllMocks();
   });
 
   it('returns all transformed projects when no filters are provided', async () => {
-    const primsaProjects: ProjectDetail[] = [
-      {
-        apiUrl: '/api/projects/1',
-        audience: '',
-        createdAt: now,
-        description: '',
-        hook: '',
-        jobs: [],
-        mediums: [],
-        members: [],
-        owner: { userId: 1 } as UserPreview,
-        projectId: 1,
-        projectImages: [],
-        projectSocials: [],
-        purpose: 'Academic' as ProjectPurpose,
-        status: 'Planning' as ProjectStatus,
-        tags: [],
-        thumbnail: null,
-        thumbnailId: 0,
-        title: 'test 1',
-        updatedAt: now,
-      },
-      {
-        apiUrl: '/api/projects/2',
-        audience: '',
-        createdAt: now,
-        description: '',
-        hook: '',
-        jobs: [],
-        mediums: [],
-        members: [],
-        owner: { userId: 2 } as UserPreview,
-        projectId: 2,
-        projectImages: [],
-        projectSocials: [],
-        purpose: 'Academic' as ProjectPurpose,
-        status: 'Planning' as ProjectStatus,
-        tags: [],
-        thumbnail: null,
-        thumbnailId: 0,
-        title: 'test 2',
-        updatedAt: now,
-      },
-    ];
-
-    const transformed: ProjectDetail[] = [
-      {
-        apiUrl: '/api/projects/1',
-        audience: '',
-        createdAt: now,
-        description: '',
-        hook: '',
-        jobs: [],
-        mediums: [],
-        members: [],
-        owner: { userId: 1 } as UserPreview,
-        projectId: 1,
-        projectImages: [],
-        projectSocials: [],
-        purpose: 'Academic' as ProjectPurpose,
-        status: 'Planning' as ProjectStatus,
-        tags: [],
-        thumbnail: null,
-        thumbnailId: 0,
-        title: 'test 1',
-        updatedAt: now,
-      },
-      {
-        apiUrl: '/api/projects/2',
-        audience: '',
-        createdAt: now,
-        description: '',
-        hook: '',
-        jobs: [],
-        mediums: [],
-        members: [],
-        owner: { userId: 2 } as UserPreview,
-        projectId: 2,
-        projectImages: [],
-        projectSocials: [],
-        purpose: 'Academic' as ProjectPurpose,
-        status: 'Planning' as ProjectStatus,
-        tags: [],
-        thumbnail: null,
-        thumbnailId: 0,
-        title: 'test 2',
-        updatedAt: now,
-      },
-    ];
-
-    (prisma.projects.findMany as Mock).mockResolvedValue(primsaProjects);
+    (prisma.projects.findMany as Mock).mockResolvedValue(prismaProjects);
     vi.mocked(transformProjectToDetail).mockImplementation((p: any) => ({
       apiUrl: p.apiUrl as string,
       audience: '',
@@ -154,11 +154,59 @@ describe('getMyProjectsService', () => {
 
     const result = await getMyProjectsService(1, 'all');
     // console.log(result);
+
     expect(vi.mocked(prisma.projects.findMany)).toHaveBeenCalled();
     expect(result).toEqual(transformed);
   });
 
-  // !!SKIPPED visibility filters (implement later?)
+  it('returns only owned projects', async () => {
+    vi.mocked(prisma.projects.findMany)
+      .mockResolvedValueOnce([prismaProjects[0]] as any) // owned
+      .mockResolvedValueOnce([prismaProjects[1]] as any); // member
+    vi.mocked(transformProjectToDetail).mockReturnValueOnce(transformed[0]);
+
+    const result = await getMyProjectsService(1, 'all', 'me');
+    // console.log(result);
+
+    expect(vi.mocked(prisma.projects.findMany)).toHaveBeenCalled();
+    expect(transformProjectToDetail).toHaveBeenCalledTimes(1);
+    expect(result).toEqual([transformed[0]]);
+  });
+
+  it('returns filtered projects based on visibility (public)', async () => {
+    vi.mocked(prisma.projects.findMany)
+      .mockResolvedValueOnce([prismaProjects[0]] as any) // all owned
+      .mockResolvedValueOnce([prismaProjects[1]] as any) // all member
+      .mockResolvedValueOnce([prismaProjects[1]] as any) // public member
+      .mockResolvedValueOnce([prismaProjects[0]] as any); // public owned
+    vi.mocked(transformProjectToDetail).mockReturnValueOnce(transformed[0]);
+
+    const result = await getMyProjectsService(1, 'public');
+    // console.log(result);
+
+    expect(vi.mocked(prisma.projects.findMany)).toHaveBeenCalled();
+    expect(transformProjectToDetail).toHaveBeenCalledTimes(2);
+    expect(result).toEqual([
+      transformed[0], // all owned
+      transformed[1], // public member
+    ]);
+  });
+
+  it('returns owned public projects', async () => {
+    vi.mocked(prisma.projects.findMany)
+      .mockResolvedValueOnce([prismaProjects[0]] as any) // all owned
+      .mockResolvedValueOnce([prismaProjects[1]] as any) // all member
+      .mockResolvedValueOnce([prismaProjects[1]] as any) // public member
+      .mockResolvedValueOnce([prismaProjects[0]] as any); // public owned
+    vi.mocked(transformProjectToDetail).mockReturnValueOnce(transformed[0]);
+
+    const result = await getMyProjectsService(1, 'public', 'me');
+    // console.log(result);
+
+    expect(vi.mocked(prisma.projects.findMany)).toHaveBeenCalled();
+    expect(transformProjectToDetail).toHaveBeenCalledTimes(1);
+    expect(result).toEqual([transformed[0]]);
+  });
 
   it('returns NOT_FOUND when no associated projects found', async () => {
     vi.mocked(prisma.projects.findMany).mockResolvedValue([]);
