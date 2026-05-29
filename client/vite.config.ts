62a699ee99177a3978fe9059432d8645ed7106d1
person973 <ddw6891@g.rit.edu>
2026-05-29 14:55:16 -0400

    Added polyfills

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,6 +1,7 @@
 /// <reference types="vitest/config" />
 import react from "@vitejs/plugin-react-swc";
 import { defineConfig, loadEnv } from "vite";
+import { nodePolyfills } from 'vite-plugin-node-polyfills';
 
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, "../", "");
@@ -25,6 +26,8 @@ export default defineConfig(({ mode }) => {
     build: {
       outDir: "build",
     },
-    plugins: [react()],
+
+    // https://vitejs.dev/config/
+    plugins: [react(), nodePolyfills()],
   };
 });
