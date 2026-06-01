75987a424aba54373b7ad9f656df0d15f2b355fc
person973 <ddw6891@g.rit.edu>
2026-06-01 16:14:09 -0400

    Added another post request to be filled in at a later date

--- server/src/api/routes/projects.ts
+++ server/src/api/routes/projects.ts
@@ -174,6 +174,18 @@ router.post(
   authenticated(requiresProjectOwner),
   PROJECT.addMember,
 );
+//Sends an invite to a pending member of a project through email
+//Currently the same as adding a member normally, but at least it doesnt break the site immediately
+router.post(
+  '/:id/members',
+  requiresLogin,
+  injectCurrentUser,
+  projectExistsAt('path', 'id'),
+  userExistsAt('body', 'userId'),
+  skipIfEmpty('body', 'roleId', attributeExistsAt('role', 'body', 'roleId')),
+  authenticated(requiresProjectOwner),
+  PROJECT.addMember,
+);
 //Edits a member of a specific project through id
 router.patch(
   '/:id/members/:userId',
