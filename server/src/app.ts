f2d5884d3dc74e86622ba864731d9e7bd27ea5d5
Kevin Zheng <kz3175@rit.edu>
2026-06-05 14:08:26 -0400

    Update app.ts

--- server/src/app.ts
+++ server/src/app.ts
@@ -46,7 +46,7 @@ app.use(
   }),
 );
 
-app.use(cors({ origin: process.env.CLIENT_URL || 'http://localhost:3000', credentials: true }));
+app.use(cors({ origin: process.env.CLIENT_URL || 'http://localhost:5173', credentials: true }));
 app.use(morgan(envConfig.env === 'development' ? 'dev' : 'tiny'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
