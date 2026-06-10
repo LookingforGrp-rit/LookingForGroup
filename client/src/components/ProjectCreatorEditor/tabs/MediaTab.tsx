21526a66ae17fa22ef89aa5eebe954bbe4796570
Echoviax <echoviax@gmail.com>
2026-06-10 09:57:28 -0400

    Add filler

--- client/src/components/ProjectCreatorEditor/tabs/MediaTab.tsx
+++ client/src/components/ProjectCreatorEditor/tabs/MediaTab.tsx
@@ -590,6 +590,24 @@ export const MediaTab = ({
         </div>
       </div>
 
+      <label style={{marginTop: "10px"}}>Project Videos</label>
+      <div className="project-editor-extra-info">
+        Provide YouTube video links to have them be embedded on your project page
+      </div>
+      <div id="project-editor-image-ui">
+        <div id="project-editor-add-image">
+          <div id="project-image-uploader" className="drop-area">
+            <div id="img-view" className="project-uploader">
+              {/* <!--!Font Awesome Free v7.2.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2026 Fonticons, Inc.--> */}
+              <svg xmlns="http://www.w3.org/2000/svg" width="38" height="38" viewBox="0 0 448 512">
+                <path d="M256 64c0-17.7-14.3-32-32-32s-32 14.3-32 32l0 160-160 0c-17.7 0-32 14.3-32 32s14.3 32 32 32l160 0 0 160c0 17.7 14.3 32 32 32s32-14.3 32-32l0-160 160 0c17.7 0 32-14.3 32-32s-14.3-32-32-32l-160 0 0-160z" fill='var(--neutral-gray)'/>
+              </svg>
+              <p className="project-editor-extra-info">Click here to add a video</p>
+            </div>
+          </div>
+        </div>
+      </div>
+
       {/* Save button */}
       <div id="general-save-info">
         <Popup>
