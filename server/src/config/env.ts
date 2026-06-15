258abf47a2acd8ecd1f4def2b5d5f6639d1d6384
Kevin Zheng <kz3175@rit.edu>
2026-06-15 13:37:05 -0400

    Update env.ts
    use https when in production

--- server/src/config/env.ts
+++ server/src/config/env.ts
@@ -1,7 +1,12 @@
+const env = (process.env.NODE_ENV ?? 'production') as 'production' | 'development' | 'test';
+
 const envConfig = Object.freeze({
-  env: (process.env.NODE_ENV ?? 'production') as 'production' | 'development' | 'test',
+  env: env,
   databaseUrl: `mysql://${process.env.DB_USER as string}:${process.env.DB_PASS as string}@${process.env.DB_HOST as string}:${process.env.DB_PORT as string}/${process.env.DB_NAME as string}`,
-  s3Endpoint: `http://${process.env.S3_HOST as string}`,
+  s3Endpoint:
+    env === 'production'
+      ? `https://${process.env.S3_HOST as string}`
+      : `http://${process.env.S3_HOST as string}`,
   s3Bucket: process.env.S3_BUCKET as string,
   s3User: process.env.S3_USER as string,
   s3Pass: process.env.S3_PASS as string,
