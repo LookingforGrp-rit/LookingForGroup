1686b98216d9569952020e2c176070593de82a2d
Kevin Zheng <kz3175@rit.edu>
2026-06-23 12:55:29 -0400

    updated invite email template
    changed the sender name to a link to their profile and the mailto link a regular text

--- server/src/services/projects/members/send-invite.ts
+++ server/src/services/projects/members/send-invite.ts
@@ -80,6 +80,8 @@ const sendInviteService = async (
 
     const inviteUrl = `${clientUrl}/acceptInvite/${String(result.requestId)}`;
 
+    const profileUrl = `${clientUrl}/profile?userID=${String(inviter.userId)}`;
+
     const receiverImg = invitee.profileImage
       ? `https://lookingforgrp.com${invitee.profileImage}`
       : 'https://lookingforgrp.com/api/images/blue_frog.png';
@@ -102,6 +104,7 @@ const sendInviteService = async (
             firstName: inviter.firstName,
             lastName: inviter.lastName,
           },
+          senderProfileLink: profileUrl,
           senderEmail: inviter.ritEmail,
           senderMessage: msg,
           projectName: project.title,
