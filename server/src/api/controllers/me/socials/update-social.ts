2200c1b280cfa6965196586bfafea39840e1c5f7
MisterFinigan <rowanluti@gmail.com>
2026-06-25 09:43:04 -0400

    fixed more type errors

--- server/src/api/controllers/me/socials/update-social.ts
+++ server/src/api/controllers/me/socials/update-social.ts
@@ -10,7 +10,7 @@ import { updateSocialService } from '#services/me/socials/update-social.ts';
 //update one of current user's social
 export const updateSocial = async (req: AuthenticatedRequest, res: Response): Promise<void> => {
   //current user ID
-  const social: UpdateUserSocialInput = { url: req.params.url };
+  const social: UpdateUserSocialInput = { url: req.params.url as string };
   const websiteId = parseInt(req.params.websiteId as string);
 
   const result = await updateSocialService({ ...social, websiteId }, req.currentUser.userId);
