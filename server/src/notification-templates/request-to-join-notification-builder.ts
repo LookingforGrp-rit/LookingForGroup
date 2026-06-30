cf6be8b8fea2433ae8a2df48f62a92d1a7662a6e
Kevin Zheng <kz3175@rit.edu>
2026-06-30 15:25:26 -0400

    Update request-to-join-notification-builder.ts

--- server/src/notification-templates/request-to-join-notification-builder.ts
+++ server/src/notification-templates/request-to-join-notification-builder.ts
@@ -76,7 +76,7 @@ export class RequestToJoinNotificationBuilder implements NotificationBuilder {
     notification.message += `You may view the requester's profile at `;
     notification.message += `<a href="${requesterProfileLink}">${requesterProfileLink}</a>. `;
     notification.message += `You may respond to their invite by going to `;
-    notification.message += `<a href=${acceptRequestLink}>${acceptRequestLink}</a>.`;
+    notification.message += `<a href="${acceptRequestLink}">${acceptRequestLink}</a>.`;
 
     return notification;
   }
