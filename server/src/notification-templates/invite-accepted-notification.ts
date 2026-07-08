eda3405681d8ddc1ec48d6b3e2269cbb0aa03f7b
MisterFinigan <rowanluti@gmail.com>
2026-07-08 10:27:50 -0400

    added a notification for when an invite to a project is accepted.

---
+++ server/src/notification-templates/invite-accepted-notification.ts
@@ -0,0 +1,61 @@
+import type { NotificationBuilderResult } from '@looking-for-group/shared';
+import type { Request } from 'express';
+import prisma from '#config/prisma.ts';
+import type { NotificationBuilder } from './notification-builder.ts';
+
+export class InviteAcceptedNotificationBuilder implements NotificationBuilder {
+  async buildNotification(request: Request): Promise<NotificationBuilderResult> {
+    const notification: NotificationBuilderResult = {
+      receiverId: 0,
+      subjectLine: '',
+      message: '',
+    };
+    const requestId = parseInt(request.params.id as string);
+
+    const data = await prisma.memberRequests.findFirst({
+      where: { requestId },
+      select: {
+        roleId: true,
+        projects: {
+          select: {
+            userId: true,
+            title: true,
+            users: {
+              select: {
+                preferredName: true,
+              },
+            },
+          },
+        },
+        users: {
+          select: {
+            preferredName: true,
+            lastName: true,
+            username: true,
+          },
+        },
+      },
+    });
+
+    if (!data) {
+      throw new Error('something caught fire.');
+    }
+
+    const roleData = await prisma.roles.findFirst({
+      where: { roleId: data.roleId },
+      select: { label: true },
+    });
+
+    const projectData = data.projects;
+    const ownerData = projectData.users;
+    const inviteeData = data.users;
+    // BUILDING THE NOTIFICATION //
+    notification.receiverId = projectData.userId;
+    notification.subjectLine = `${inviteeData.preferredName} has accepted your invitation to join ${projectData.title}`;
+
+    notification.message = `Hello ${ownerData.preferredName},<br /><br />`;
+    notification.message += `${inviteeData.preferredName} has accepted your invitation to join ${projectData.title} `;
+    notification.message += `as a ${roleData?.label as string}. You may also assign them to other roles if need be.`;
+    return notification;
+  }
+}
