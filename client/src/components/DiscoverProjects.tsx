668ee4193e1d55b5558465c516a6607f4cc672d2
Mateusz Wargacki <114600369+TeaSunflower@users.noreply.github.com>
2026-07-09 13:13:15 -0400

    Moved right filter arrow in front of more filters button

--- client/src/components/DiscoverProjects.tsx
+++ client/src/components/DiscoverProjects.tsx
@@ -370,6 +370,13 @@ export const DiscoverProjects: React.FC<DiscoverProjectsProps> = ({ updateItemLi
                         )
                     })}
                 </div>
+                <button
+                    id="filters-right-scroll"
+                    className={`filters-scroller ${!showRightArrow ? 'hide' : ''}`}
+                    onClick={() => scrollTags('right')}
+                >
+                    <i className="fa fa-caret-right"></i>
+                </button>
                 {/* Container so more filters popup is aligned at the end */}
                 <div id="discover-more-filters-container">
 
@@ -648,13 +655,6 @@ export const DiscoverProjects: React.FC<DiscoverProjectsProps> = ({ updateItemLi
                         </PopupContent>
                     </Popup>
                 </div>
-                <button
-                    id="filters-right-scroll"
-                    className={`filters-scroller ${!showRightArrow ? 'hide' : ''}`}
-                    onClick={() => scrollTags('right')}
-                >
-                    <i className="fa fa-caret-right"></i>
-                </button>
             </div >
             {((appliedFiltersDisplay.length > 0) && (displayFiltersText)) ? (
                 <div className='applied-filters'>
