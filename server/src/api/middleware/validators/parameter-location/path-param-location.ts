e15a33f45a69bd1148e3d8f4c04b58967e81ef9b
MisterFinigan <rowanluti@gmail.com>
2026-07-13 11:52:23 -0400

    Added parameter location classes

---
+++ server/src/api/middleware/validators/parameter-location/path-param-location.ts
@@ -0,0 +1,23 @@
+import type { ApiResponse } from '@looking-for-group/shared';
+import type { Request } from 'express';
+import type { ParameterLocation } from './parameter-location.ts';
+
+/**
+ * Looks for an ID in the path.
+ */
+export class PathParameterLocation implements ParameterLocation {
+  getId(key: string, request: Request): number | ApiResponse {
+    const res: ApiResponse = { status: 0 };
+
+    const rawTargetId = request.params[key] as string;
+
+    const targetId = parseInt(rawTargetId);
+    if (isNaN(targetId)) {
+      res.status = 400;
+      res.error = 'Invalid user id.';
+      return res;
+    }
+
+    return targetId;
+  }
+}
