8130850c98985ee71d6d38897c253c7857e8dbcc
MisterFinigan <rowanluti@gmail.com>
2026-07-28 10:36:07 -0400

    overhauled requiresNotSelf, made it so you cannot perform mod actions on projects you are a part of.

--- server/src/api/middleware/authorization/requires-not-self.ts
+++ server/src/api/middleware/authorization/requires-not-self.ts
@@ -5,25 +5,41 @@ import type { ParameterLocation } from '#middleware/validators/parameter-locatio
 /**
  * Checks if the subject of the action being performed is the same person as the one performing the action.
  *  (e.g. moderator approving their own project)
+ * @param subjectParamLocations A map of parameter keys ("id", "userId", etc) to parameter locations (path, body, etc.)
  */
-export const requiresNotSelf = (subjectParamLocation: ParameterLocation, subjectKey: string) => {
+export const requiresNotSelf = (subjectParamLocations: Map<ParameterLocation, string>) => {
   return async (request: AuthenticatedRequest, response: Response, next: NextFunction) => {
-    const subjectResult = await subjectParamLocation.getId(subjectKey, request);
-    if (typeof subjectResult !== 'number') {
-      response.status(subjectResult.status).json(subjectResult);
-      return;
-    }
+    // Getting all ids
+    const promises: Promise<number[] | ApiResponse>[] = [];
 
+    subjectParamLocations.forEach((value, key) => {
+      promises.push(key.getId(value, request));
+    });
+
+    const ids = await Promise.all(promises);
+
+    // Checking all results
     const meId = request.currentUser.userId;
 
-    if (meId === subjectResult) {
-      const res: ApiResponse = {
-        status: 403,
-        error: 'You cannot do this yourself.',
-      };
-      response.status(res.status).json(res);
-      return;
-    }
+    ids.forEach((subjectResult) => {
+      if ('status' in subjectResult) {
+        response.status(subjectResult.status).json(subjectResult);
+        return;
+      }
+
+      // This is typically just one id, but in some cases (e.g. looking through a list of project members)
+      //      we have to loop through an array.
+      subjectResult.forEach((id) => {
+        if (meId === id) {
+          const res: ApiResponse = {
+            status: 403,
+            error: 'You cannot do this yourself.',
+          };
+          response.status(res.status).json(res);
+          return;
+        }
+      });
+    });
 
     next();
   };
